09528b0a4a18483397ea964a2e484e970ce43912
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2017-10-25 22:20:07 +0300

    node-server backup

---
+++ src/node/websock/node_server/sockets/node-server.js
@@ -0,0 +1,109 @@
+let io = require('socket.io');
+
+import {nodeVersionCompatibility, nodeVersion, nodePort} from '../../../../consts/const_global.js';
+import {SocketExtend} from '../../../../common/sockets/socket-extend.js';
+import {NodeLists} from '../../../lists/node-lists.js';
+import {NodeProtocol} from '../../../../common/sockets/protocol/node-protocol.js';
+import {NodePropagationProtocol} from '../../../../common/sockets/protocol/node-propagation-protocol.js';
+
+/*
+    TUTORIAL
+
+    socket.emit('request', {); // emit an event to the socket
+    io.emit('broadcast', {); // emit an event to all connected socket
+    socket.on('reply', function(){  }); // listen to the event
+ */
+
+class NodeServer {
+
+    /*
+        nodeServer : null,        //Node IO Server Socket
+    */
+
+    constructor(){
+
+        console.log("NodeServer constructor");
+        this.nodeServer = null;
+
+    }
+
+    startServer(){
+
+        this.nodeServer = null;
+
+        try
+        {
+            let server = null;
+            try {
+                console.log(typeof io);
+                server = io();
+
+            } catch(Exception){
+                console.log("Error Importing io() library", Exception.toString());
+            }
+            this.nodeServer = server;
+
+            server.on("connection", socket => {
+
+                SocketExtend.extendSocket(socket, socket.request.connection.remoteAddress, socket.request.connection.remotePort);
+
+                console.log('New connection from ' + socket.node.sckAddress.toString() + ':' + socket.node.sckAddress.port);
+
+                socket.node.protocol.sendHello().then( (answer)=>{
+                    this.initializeSocket(socket);
+                });
+
+            });
+
+            try {
+                console.log(typeof server);
+                server.listen(nodePort);
+            } catch(Exception){
+                console.log("Error Calling node_server.listen", Exception.toString());
+            }
+
+        }
+        catch(Exception){
+            console.log("Error Starting Node Server ", Exception.toString());
+            return false;
+        }
+
+        console.log("Node Server Started");
+        return true;
+    }
+
+
+
+    initializeSocket(socket){
+
+        //it is not unique... then I have to disconnect
+        if (NodeLists.addUniqueSocket(socket, "server") === false){
+            return false;
+        }
+
+
+        socket.once("disconnect", answer => {
+            console.log("Socket disconnected", socket.sckAddress.getAddress());
+            NodeLists.disconnectSocket(socket);
+        });
+
+        this.initializeWebRTCSignals(socket);
+
+        NodePropagationProtocol.initializeSocketForPropagation(socket);
+    }
+
+
+    initializeWebRTCSignals(socket){
+
+        socket.on("pools/signal/initialize", answer =>{
+
+        });
+
+        socket.on("pools/signal/join", answer =>{
+
+        });
+    }
+
+}
+
+exports.NodeServer = new NodeServer();
