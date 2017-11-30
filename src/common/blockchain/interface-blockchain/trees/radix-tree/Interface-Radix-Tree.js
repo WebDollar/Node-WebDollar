8080b81fece3e7fbbea03e2c69c419a4a407886c
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2017-11-30 14:46:11 +0200

    Radix Tree Add, Delete & Search

--- src/common/blockchain/interface-blockchain/trees/radix-tree/Interface-Radix-Tree.js
+++ src/common/blockchain/interface-blockchain/trees/radix-tree/Interface-Radix-Tree.js
@@ -1,5 +1,10 @@
 /*
-    Used to Radix Tree a Buffer
+    Used to Radix Tree a Buffer (CryptoWebDollarData)
+
+    Tutorials:
+        https://en.wikipedia.org/wiki/Radix_tree
+
+        https://www.cs.usfca.edu/~galles/visualization/RadixTree.html   - animated demo
  */
 
 import WebDollarCryptData from "common/crypto/Webdollar-Crypt-Data";
@@ -16,7 +21,7 @@ class InterfaceRadixTree {
     }
 
     /**
-     *
+     * Adding an input to the Radix Tree
      * @param element can be a Base String, Buffer or CryptoWebDollarData
      */
     radixAdd(input, value){
@@ -25,29 +30,105 @@ class InterfaceRadixTree {
 
         let nodeCurrent = this.radixRoot;
 
-        for (let i=0; i<input.length; i++){
+        for (let i=0; i<input.buffer.length; i++){
 
             //searching for existence of input[i] in nodeCurrent list
 
+            let childFound = false;
             for (let j=0; j<nodeCurrent.edges.length; j++)
-
                 //we found the targetNode of label input[i]
-                if (nodeCurrent.edges[j].label === input[i] ) {
+                if (nodeCurrent.edges[j].label === input.buffer[i] ) {
                     nodeCurrent = nodeCurrent.edges[j].targetNode;
-                    break;
-                } else
-                if (nodeCurrent.edges[j].label !== input[i] ) {
-                    //creating a new node
-                    let nodeChild = new InterfaceRadixTreeNode(nodeCurrent,  i === (input.length-1) ? value : null, [] );
-
-                    nodeCurrent.edges.push( new InterfaceRadixTreeEdge(input[i], nodeChild));
-
-                    nodeCurrent = nodeChild;
+                    childFound = true;
                     break;
                 }
+
+            if (!childFound){
+                //creating a new node
+                let nodeChild = new InterfaceRadixTreeNode(nodeCurrent,  i === (input.buffer.length-1) ? value : null, [] );
+
+                nodeCurrent.edges.push( new InterfaceRadixTreeEdge(input.buffer[i], nodeChild));
+
+                nodeCurrent = nodeChild;
+            }
+
         }
 
+        //nodeCurrent will be the last child added in the list
+        return nodeCurrent;
 
+    }
+
+    /**
+     *
+     * @param input
+     */
+    radixDelete(input){
+
+        input = WebDollarCryptData.createWebDollarCryptData(input)
+
+        let searchResult = this.radixSearch(input);
+
+        if (searchResult.result === false || searchResult.node === null) return false;
+
+        //it is the last element, we should delete it
+        if ( searchResult.index === input.buffer.length-1){
+            let nodeChild = searchResult.node;
+            let nodeParent = searchResult.node.parent;
+
+            nodeChild.value = null;
+
+            //remove empty parent nodes
+            while (nodeChild !== null && nodeParent !== null && nodeChild.edges.length === 0){
+
+                //removing edge to child from parent
+                for (let i=0; i<nodeParent.edges.length; i++)
+                    if (nodeParent[i].targetNode === nodeChild) {
+                        nodeParent.edges.splice(i, 1);
+                        break;
+                    }
+
+                nodeChild = nodeParent;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+
+    /**
+     * Searching an input in the Radix Tree
+     * @param input
+     */
+    radixSearch(input){
+
+        input = WebDollarCryptData.createWebDollarCryptData(input)
+
+        let nodeCurrent = this.radixRoot;
+
+        for (let i=0; i<input.buffer.length; i++){
+
+            //searching for existence of input[i] in nodeCurrent list
+
+            let childFound = false;
+            for (let j=0; j<nodeCurrent.edges.length; j++)
+                //we found the targetNode of label input[i]
+                if (nodeCurrent.edges[j].label === input.buffer[i] ) {
+                    nodeCurrent = nodeCurrent.edges[j].targetNode;
+                    childFound = true;
+                    break;
+                }
+
+            if (!childFound){
+                return {result: false, index: -1, node: null};
+            }
+
+        }
+
+        //nodeCurrent will be the last child added in the list
+        return {result: true, index: input.buffer.length, edgeIndex: j, node: nodeCurrent};
     }
 
 }
