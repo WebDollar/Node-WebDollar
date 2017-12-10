eff55e6c70edff6a3d8e6bc44cc1607f23aabcbf
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2017-12-10 17:48:11 +0200

    Interface Block

--- src/common/blockchain/interface-blockchain/blocks/Interface-Blockchain-Block.js
+++ src/common/blockchain/interface-blockchain/blocks/Interface-Blockchain-Block.js
@@ -9,16 +9,15 @@ class InterfaceBlockchainBlock{
 
     constructor (){
 
-        this.version = null; // 4 bytes
+        this.version = null; // version                                                         - 2 bytes
 
-        this.prevHash = null; //32 bytes, sha256
+        this.hashPrev = null; //256-bit hash sha256                                             - 32 bytes, sha256
 
-        this.hash = null; // 256-bit hash based on all of the transactions in the block - 32 bytes, sha256
+        this.hashData = null; // 256-bit hash based on all of the transactions in the block     - 32 bytes, sha256
 
-        this.nonce = null; //	32-bit number (starts at 0)-  int,                       4 bytes
+        this.nonce = null; //	32-bit number (starts at 0)-  int,                              - 4 bytes
 
-        this.time = null; //Current timestamp as seconds since 1970-01-01T00:00 UTC   -  4 bytes,
-
+        this.timeStamp = null; //Current timestamp as seconds since 1970-01-01T00:00 UTC        - 4 bytes,
 
         this.data = null; // transactions - data
 
@@ -26,26 +25,35 @@ class InterfaceBlockchainBlock{
 
     validateBlock(){
 
+        if (typeof this.version === 'undefined' || this.version === null) throw ('version is empty');
+
+        if (typeof this.hashPrev === 'undefined' || this.hashPrev === null) throw ('hashPrev is empty');
         if (typeof this.data === 'undefined' || this.data === null) throw ('data is empty');
-        if (typeof this.prevHash === 'undefined' || this.prevHash === null) throw ('prevHash is empty');
-        if (typeof this.hash === 'undefined' || this.hash === null) throw ('hash is empty');
+        if (typeof this.hashData === 'undefined' || this.hashData === null) throw ('hashData is empty');
+
         if (typeof this.nonce === 'undefined' || this.nonce === null) throw ('nonce is empty');
+        if (typeof this.timeStamp === 'undefined' || this.timeStamp === null) throw ('timeStamp is empty');
+
+        if (this.version !== 0x0666) throw ('invalid version');
 
         return true;
     }
 
     toString(){
 
-        return this.hash.toString() + this.prevHash.toString() + JSON.stringify(this.data);
+        return this.hashData.toString() + this.hashPrev.toString() + JSON.stringify(this.data);
 
     }
 
     toJSON(){
 
         return {
-            hash: this.hash,
-            prevHash: this.prevHash,
+            version: this.version,
+            hashPrev: this.hashPrev,
+            hashData: this.hashData,
             data: this.data,
+            nonce: this.nonce,
+            timeStamp: this.timeStamp,
         }
 
     }
