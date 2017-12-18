f6c77528645e4ba5f9f2f9669bb34ec96ca535e9
Stroe Silviu Bogdan <silviu@silviu-s.com>
2017-12-18 22:54:33 +0200

    - fixed things - implemented save, get and remove methods from both objects and buffer - browser detection - maybe need some refactoring - tests needed

--- src/common/pouchdb/Interface-PouchDB.js
+++ src/common/pouchdb/Interface-PouchDB.js
@@ -1,4 +1,4 @@
-/* Added by Silviu Stroe - www.silviu-s.com */
+/* Added by Silviu Bogdan Stroe - https://www.silviu-s.com */
 const PouchDB = require('pouchdb-node');
 
 class InterfacePouchDB {
@@ -6,9 +6,13 @@ class InterfacePouchDB {
     constructor(databaseName) {
         this.dbName = (typeof databaseName !== 'undefined' && databaseName !== null ? databaseName : "defaultDB");
         this.db = new PouchDB(this.dbName);
+        this.attachName = 'wallet.bin';
+        this.isBrowser = this === window;
     }
 
-    save(key, value) {
+    createDocument(key, value) {
+        this.deleteAttachmentIfExist(key);
+
         return this.db.put({
             _id: key,
             value: value
@@ -17,15 +21,15 @@ class InterfacePouchDB {
         }).catch((err) => {
             //document exists, update it
             if (err.status === 409) {
-                return this.update(key, value)
+                return this.updateDocument(key, value)
             } else {
-                return err;
+                throw err;
             }
         });
     }
 
 
-    update(key, value) {
+    updateDocument(key, value) {
         return this.db.get(key).then((doc) => {
             return this.db.put({
                 _id: doc._id,
@@ -35,78 +39,123 @@ class InterfacePouchDB {
         }).then((response) => {
             return response;
         }).catch((err) => {
-            return err;
+            throw err;
         });
     }
 
-    get(key) {
+    getDocument(key) {
         return this.db.get(key).then((doc) => {
-            return doc;
+            return doc.value;
         }).catch((err) => {
             console.log(err);
         });
     }
 
-    del(key) {
+    deleteDocument(key) {
         return this.db.get(key).then((doc) => {
-            return this.db.remove(doc);
+            return this.db.remove(doc._id, doc._rev);
         }).then((result) => {
             return result;
         }).catch((err) => {
-            return err;
+            throw err;
         });
     }
 
     //attachments
 
     saveAttachment(key, value) {
-        let attachment = new Buffer(value);
-        return this.db.putAttachment(key, 'wallet.bin', attachment).then((result) => {
-            return result;
+        // this.deleteDocumentIfExist(key);
+
+        let attachment = (this.isBrowser) ? new Blob(value, {content_type: 'application/octet-stream'}) : new Buffer(value, {content_type: 'application/octet-stream'});
+
+
+        return this.db.putAttachment(key, this.attachName, attachment).then((result) => {
+            return result.ok;
         }).catch((err) => {
             //document exists, update it
             if (err.status === 409) {
                 return this.updateAttachment(key, value);
             } else {
-                return err;
+                if (err.status === 404) {
+                    //if document not exist, create it and recall attachment
+                    return this.createDocument(key, null).then(() => this.saveAttachment(key, value));
+                } else {
+                    throw err;
+                }
             }
         });
     }
 
-    getattachment(key) {
-        return this.db.getAttachment(key, 'wallet.bin').then((blobOrBuffer) => {
+    getAttachment(key) {
+        return this.db.getAttachment(key, this.attachName).then((blobOrBuffer) => {
             return blobOrBuffer;
         }).catch((err) => {
-            return err;
+            throw err;
         });
     }
 
     updateAttachment(key, value) {
         return this.db.get(key).then((doc) => {
-            let attachment = new Buffer(value);
-            return this.db.putAttachment(doc._id, 'wallet.bin', doc._rev, attachment).then((result) => {
-                return result;
+            let attachment = (this.isBrowser) ? new Blob(value, {content_type: 'application/octet-stream'}) : new Buffer(value, {content_type: 'application/octet-stream'});
+
+            return this.db.putAttachment(doc._id, this.attachName, doc._rev, attachment, 'application/octet-stream').then((result) => {
+                return result.ok;
             }).catch((err) => {
-                return err;
+                throw err;
             });
 
         }).catch((err) => {
-            return err;
+            throw err;
         });
     }
 
     deleteAttachment(key) {
         return this.db.get(key).then((doc) => {
-            return this.db.removeAttachment(doc._id, 'wallet.bin', doc._rev).then((result) => {
+            return this.db.removeAttachment(doc._id, this.attachName, doc._rev).then((result) => {
                 return result;
             }).catch((err) => {
-                return err;
+                throw err;
             });
         }).catch((err) => {
-            return err;
+            throw err;
         });
     }
 
+
+    deleteAttachmentIfExist(key) {
+        return this.getAttachment(key).then((value) => {
+            return this.deleteAttachment(key);
+        }).catch((err) => {
+            return false;
+        })
+    }
+
+
+    //general methods
+
+    save(key, value) {
+        if (typeof value === "object") {
+            return this.createDocument(key, value);
+        } else {
+            return this.saveAttachment(key, value);
+        }
+    }
+
+    get(key) {
+        return this.getDocument(key).then(() => {
+            return this.getAttachment(key).then(() => {
+                return this.getAttachment(key);
+            }).catch(() => {
+                return this.getDocument(key);
+            });
+        }).catch((err) => {
+            throw err;
+        })
+    }
+
+    remove(key) {
+        return this.deleteDocument(key);
+    }
 
 }
 
