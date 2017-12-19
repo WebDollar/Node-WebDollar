f226688c41e915e6b7e023dee23d8e2ce18efe9c
Stroe Silviu Bogdan <silviu@silviu-s.com>
2017-12-19 11:47:25 +0200

    - fixed things - implemented save, get and remove methods from both objects and buffer - browser detection - maybe need some refactoring - tests needed

--- src/common/pouchdb/Interface-PouchDB.js
+++ src/common/pouchdb/Interface-PouchDB.js
@@ -129,7 +129,7 @@ class InterfacePouchDB {
     //main methods
 
     save(key, value) {
-        if (typeof value === "object") {
+        if (typeof value === "object" || typeof value === "string") {
             return this.createDocument(key, value);
         } else {
             return this.saveDocumentAttachment(key, value);
