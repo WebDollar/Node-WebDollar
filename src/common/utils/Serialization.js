f2ea4546f6f18b8a98aa438c2952829c7b6d5e4a
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2017-12-24 05:26:29 +0200

    Serialization Big Number

--- src/common/utils/Serialization.js
+++ src/common/utils/Serialization.js
@@ -1,10 +1,32 @@
+var BigNumber = require('bignumber.js');
+
 class Serialization{
 
     /**
      * Serialize a Big Number object into an optimal Buffer
      */
-    static serializeBigNumber(bigNumber){
+    static serializeBigNumber(data){
+        //9999999999.99999999999
+        // d: [999, 9999999, 9999999, 9999000]
+        // d biggest number is 0x98967F
+        // e: 9  - it can also be negative
+        // s: 1
 
+        if (! data instanceof BigNumber) throw 'data is not big number';
+        if ( data.d.length === 0 ) throw "data is 0 and can't be ";
+
+        let buffer = new Buffer( 1 + data.d.length * 3 + 1 );
+
+        buffer[0] = data.e % 256;
+        buffer[1] = data.d.length % 128 + Math.sign(data.e)*128;
+
+        for (let i=0; i<data.d.length; i++) {
+            buffer[2 + (i * 3 + 2)]   = data.d[i] & 0xff;
+            buffer[2 + (i * 3 + 1 )]  = data.d[i] >> 8 & 0xff;
+            buffer[2 + (i * 3 )]      = data.d[i] >> 16 & 0xff;
+        }
+
+        return buffer;
     }
 
     /**
@@ -12,6 +34,22 @@ class Serialization{
      */
     static deserializeBigNumber(buffer){
 
+        let bigNumber = {e:0, sign:0, d: []};
+
+        if (!Buffer.isBuffer(buffer)) throw "Can't deserialize Big Number because it is not a buffer";
+
+        bigNumber.e = buffer[0];
+        let length = buffer[1] % 128;
+        let sign = buffer[1] / 128;
+
+        if (sign > 0)  bigNumber.e = - bigNumber.e;
+
+        for (let i=0; i<length; i++){
+            let nr = buffer[2+i*3] & 0xff + buffer[2+i*3 + 1]  >> 8 & 0xff + buffer[2+i*3 + 2] >> 16 & 0xff;
+            bigNumber.d.push(nr);
+        }
+
+        return new BigNumber(bigNumber)
     }
 
     /**
@@ -41,10 +79,10 @@ class Serialization{
     static serializeNumber4Bytes(data){
         //converting number value into a buffer
         let buffer = Buffer(4);
-        buffer[0] = (data & 0xff);
-        buffer[1] = (data>>8 & 0xff);
-        buffer[2] = (data>>16 & 0xff);
-        buffer[3] = (data>>24 & 0xff);
+        buffer[0] = data & 0xff;
+        buffer[1] = data>>8 & 0xff;
+        buffer[2] = data>>16 & 0xff;
+        buffer[3] = data>>24 & 0xff;
 
         return  buffer;
     }
