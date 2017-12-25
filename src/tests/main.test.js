e6086debb00cccf20a92936c036403dffc1a1003
Cosmin-Dumitru Oprea <opreacosmind@gmail.com>
2017-12-26 01:29:50 +0200

    main.test: Correction
    Signed-off-by: Cosmin-Dumitru Oprea <opreacosmind@gmail.com>

--- src/tests/main.test.js
+++ src/tests/main.test.js
@@ -6,25 +6,25 @@ if(( typeof window !== 'undefined' && !window._babelPolyfill) ||
 if ( typeof describe !== 'undefined') {
 
 
-    //require ('tests/blockchain/interface-blockchain/Interface-Blockchain-Address.test');
-    //require ('tests/blockchain/interface-blockchain/Interface-Blockchain-Difficulty.test');
-    //require ('tests/blockchain/interface-blockchain/Interface-Blockchain-Block-LocalStore.test');
-    //require ('tests/blockchain/protocol/Interface-Blockchain-Protocol-Fork-Solver.test');
+    require ('tests/blockchain/interface-blockchain/Interface-Blockchain-Address.test');
+    require ('tests/blockchain/interface-blockchain/Interface-Blockchain-Difficulty.test');
+    require ('tests/blockchain/interface-blockchain/Interface-Blockchain-Block-LocalStore.test');
+    require ('tests/blockchain/protocol/Interface-Blockchain-Protocol-Fork-Solver.test');
 
-    //require ('tests/satoshmindb/Interface-SatoshminDB.test');
+    require ('tests/satoshmindb/Interface-SatoshminDB.test');
     require ('tests/main-blockchain/Main-Blockchain-Wallets.test');
 
-    //require ('tests/trees/radix/Interface-Radix-Tree.test');
-    //require ('tests/trees/radix/accountant/Interface-Accountant-Radix-Tree.test');
+    require ('tests/trees/radix/Interface-Radix-Tree.test');
+    require ('tests/trees/radix/accountant/Interface-Accountant-Radix-Tree.test');
 
-    //require ('tests/trees/Interface-Merkle-Tree.test');
-    //require ('tests/trees/radix/merkle-tree/Interface-Merkle-Radix-Tree.test');
-    //require ('tests/trees/radix/accountant/merkle-tree/Interface-Merkle-Accountant-Radix-Tree.test');
+    require ('tests/trees/Interface-Merkle-Tree.test');
+    require ('tests/trees/radix/merkle-tree/Interface-Merkle-Radix-Tree.test');
+    require ('tests/trees/radix/accountant/merkle-tree/Interface-Merkle-Accountant-Radix-Tree.test');
 
-    //require ('tests/crypto/WebDollar-Crypto.test');
-    //require ('tests/crypto/Argon2.test');
+    require ('tests/crypto/WebDollar-Crypto.test');
+    require ('tests/crypto/Argon2.test');
 
-	//require ('tests/big-number/Interface-BigNumber.test');
+	require ('tests/big-number/Interface-BigNumber.test');
 
     //require ('tests/blockchain/Node-Web-Peer.test')
 
