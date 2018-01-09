17b171a642de0af4be1dc6ebe961ecc1280d1c30
Stroe Silviu Bogdan <silviu@silviu-s.com>
2018-01-09 19:16:26 +0200

    - generate address from 3 public keys

--- src/common/blockchain/interface-blockchain/addresses/Interface-Blockchain-Address-Helper2.js
+++ src/common/blockchain/interface-blockchain/addresses/Interface-Blockchain-Address-Helper2.js
@@ -5,34 +5,34 @@ const bitcoin = require('bitcoinjs-lib');
 
 class InterfaceBlockchainAddressHelper2 {
 
+    /**
+     *
+     * @param publicKeys
+     * @returns {*}
+     *
+     * Example:
+     *
+     let pubKeys = [
+     '026477115981fe981a6918a6297d9803c4dc04f328f22041bedff886bbc2962e01',
+     '02c96db2302d19b43d4c69368babace7854cc84eb9e061cde51cfa77ca4a22b8b9',
+     '03c6103b3b83e4a24a0e33a4df246ef11772f9992663db0c35759a5e2ebf68d8e9'
+     ];
+     let address = InterfaceBlockchainAddressHelper2.generateAddress(pubKeys);
+     */
+    static generateAddress(publicKeys) {
 
-    static generateAddress(key_no = 3) {
-
-
-        let privateKeys = [];
-        for (let x = 0; x < key_no; x++) {
-            privateKeys.push(bitcoin.ECKey.makeRandom())
-        }
-
-        let publicKeys = privateKeys.map(function (x) {
-            return x.pub
+        let pubKeys = publicKeys.map(function (hex) {
+            return Buffer.from(hex, 'hex')
         });
 
+        let redeemScript = bitcoin.script.multisig.output.encode(2, pubKeys); // 2 of 3
+        let scriptPubKey = bitcoin.script.scriptHash.output.encode(bitcoin.crypto.hash160(redeemScript));
+        let address = bitcoin.address.fromOutputScript(scriptPubKey);
 
-        let redeemScript = bitcoin.scripts.multisigOutput(2, publicKeys);
-        let scriptPubKey = bitcoin.scripts.scriptHashOutput(redeemScript.getHash());
-        let address = bitcoin.Address.fromOutputScript(scriptPubKey).toString();
 
-        let res = {};
-        res.address = address;
-        res.redeemScript = redeemScript.toHex();
-        res.privateKeys = privateKeys.map(function (x) {
-            return x.toWIF()
-        });
-
-        return res;
+        return address;
     }
 
 }
 
-export default InterfaceBlockchainAddressHelper2;
+module.exports = InterfaceBlockchainAddressHelper2;
