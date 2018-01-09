168d76731c3595516cb5bda98aae7473cd833860
Stroe Silviu Bogdan <silviu@silviu-s.com>
2018-01-09 19:43:25 +0200

    create transactions in progress

--- src/common/blockchain/interface-blockchain/addresses/Interface-Blockchain-Address-Helper2.js
+++ src/common/blockchain/interface-blockchain/addresses/Interface-Blockchain-Address-Helper2.js
@@ -33,6 +33,49 @@ class InterfaceBlockchainAddressHelper2 {
         return address;
     }
 
+    /**
+     * create (and broadcast via 3PBP) a Transaction with a 2-of-4 P2SH(multisig) input
+     * https://github.com/bitcoinjs/bitcoinjs-lib/blob/e0f24fdd46e11533a7140e02dc43b04a4cc4522e/test/integration/transactions.js#L115
+     */
+    createTransaction() {
+
+        let keyPairs = [
+            '91avARGdfge8E4tZfYLoxeJ5sGBdNJQH4kvjJoQFacbgwmaKkrx',
+            '91avARGdfge8E4tZfYLoxeJ5sGBdNJQH4kvjJoQFacbgww7vXtT',
+            '91avARGdfge8E4tZfYLoxeJ5sGBdNJQH4kvjJoQFacbgx3cTMqe',
+            '91avARGdfge8E4tZfYLoxeJ5sGBdNJQH4kvjJoQFacbgx9rcrL7'
+        ].map(function (wif) {
+            return bitcoin.ECPair.fromWIF(wif, testnet)
+        })
+        let pubKeys = keyPairs.map(function (x) {
+            return x.getPublicKeyBuffer()
+        })
+
+        let redeemScript = bitcoin.script.multisig.output.encode(2, pubKeys)
+        let scriptPubKey = bitcoin.script.scriptHash.output.encode(bitcoin.crypto.hash160(redeemScript))
+        let address = bitcoin.address.fromOutputScript(scriptPubKey, testnet)
+
+        testnetUtils.faucet(address, 2e4, function (err, unspent) {
+            if (err) return done(err)
+
+            let txb = new bitcoin.TransactionBuilder(testnet)
+            txb.addInput(unspent.txId, unspent.vout)
+            txb.addOutput(testnetUtils.RETURN_ADDRESS, 1e4)
+
+            txb.sign(0, keyPairs[0], redeemScript)
+            txb.sign(0, keyPairs[2], redeemScript)
+
+            let tx = txb.build()
+
+            // build and broadcast to the Bitcoin Testnet network
+            testnetUtils.transactions.propagate(tx.toHex(), function (err) {
+                if (err) return done(err)
+
+                testnetUtils.verify(address, tx.getId(), 1e4, done)
+            })
+        })
+    }
+
 }
 
 module.exports = InterfaceBlockchainAddressHelper2;
