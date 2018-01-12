a91675056b1414668d5c2d0c1462c79c35ce773d
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-01-12 22:20:23 +0200

    Backbone Mining

--- src/common/blockchain/interface-blockchain/mining/backbone/Interface-Blockchain-Backbone-Mining.js
+++ src/common/blockchain/interface-blockchain/mining/backbone/Interface-Blockchain-Backbone-Mining.js
@@ -5,6 +5,68 @@ class InterfaceBlockchainBackboneMining extends InterfaceBlockchainMining {
 
     //backbone mining is the same with InterfaceBlockchainMining
 
+    constructor(blockchain, minerAddress){
+
+        super(blockchain, minerAddress);
+
+        this.WORKER_NONCES_WORK = 200;
+
+        this.block = undefined;
+        this.undefined = undefined;
+        this._workerResolve = undefined;
+    }
+
+    async mineNonces(){
+
+        for (let i=0; i<this.WORKER_NONCES_WORK; i++) {
+
+            if (this._nonce > 0xFFFFFFFF || !this.started || this.reset ){
+                this._workerResolve({result:false});
+                return false;
+            }
+
+            let hash = await this.block.computeHash(this._nonce);
+
+            //console.log('Mining WebDollar Argon2 - this._nonce', this._nonce, hash.toString("hex") );
+
+            if ( hash.compare(this.difficulty) <= 0 ) {
+
+                this._workerResolve( {
+                    result:true,
+                    nonce: this._nonce,
+                    hash: hash,
+                } );
+
+                return;
+            }
+
+            this._nonce++;
+            this._hashesPerSecond++;
+
+        }
+
+
+        setTimeout( async () => { return await this.mineNonces() }, 10);
+
+    }
+
+    mine(block, difficultyTarget){
+
+        this.block = block;
+        this.difficulty = difficultyTarget;
+
+        let promiseResolve = new Promise ( (resolve)=>{
+
+            this._workerResolve = resolve;
+            setTimeout( async () => { return await this.mineNonces() }, 10);
+
+        } );
+
+        return promiseResolve;
+
+    }
+
+
 }
 
 export default InterfaceBlockchainBackboneMining
