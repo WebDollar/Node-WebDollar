24a1bd2e1488c3a7603aa4d3c942518a9346231e
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-01-13 14:28:24 +0200

    NiPoPoW compareProofs and BestArg

--- src/common/blockchain/ppow-blockchain/blockchain/PPoW-Blockchain.js
+++ src/common/blockchain/ppow-blockchain/blockchain/PPoW-Blockchain.js
@@ -4,7 +4,7 @@ import consts from 'consts/const_global'
 import BlockchainGenesis from 'common/blockchain/global/Blockchain-Genesis'
 
 /**
- * PPoWBlockchain contains a chain of blocks based on Proof of Proofs of Work
+ * NiPoPoW Blockchain contains a chain of blocks based on Proof of Proofs of Work
  */
 class PPoWBlockchain extends InterfaceBlockchain {
 
@@ -45,35 +45,58 @@ class PPoWBlockchain extends InterfaceBlockchain {
 
     }
 
-    //Algorithm 4 aka bestArg
-    compareProofs(proof1, proof2){
+    LCA(proofs1, proofs2){
+        //LCA(C1, C2) = (C1 ∩ C2)[−1]
 
-        let bestArg = (proof, b) => {
+        for (let i=proofs1.length-1; i > 0; i--)
+            for (let j=proofs2.length-1; j>0; j--){
+                if (proofs1[i] === proofs2[j]){ //found the LCA
+                    return proofs1[i];
+                }
+            }
+
+        return null;
+
+    }
+
+    //Algorithm 4 aka bestArg
+    compareProofs(proofs1, proofs2){
+
+        let bestArg = (proofs, b) => {
 
             //M ← {µ : |π↑µ {b :}| ≥ m} ∪ {0}
-            //let M =
+
+            // Obs M is a level
+            let M = [0];
+
+            for (let miu = proofs.length-1; miu >= 0; i-- ) {
+
+                // { b : }
+                if (proofs[miu] === b) //finished,
+                    break;
+
+                // {µ : |π↑µ {b :}| ≥ m}
+                if ( proofs[miu].length >= consts.POPOW_PARAMS.m ) M.push( miu );
+            }
 
             //return max µ∈M {2^µ · | π↑µ {b : }| }
-            //return max
-        }
+            let max = 0;
+            for (let miu in M){
 
-        //operator
-        let intersection = [];
+                let formula = 2^miu * proofs[miu].length;
+                if ( max < formula )
+                    max = formula;
+
+            }
+
+            return max
+        };
 
         //calculating the interesection
-        for (let i=0; i<proof1.length; i++)
-            for (let j=0; j<proof2.length; j++)
-                if (proof1[i] === proof2[j]){
-                    intersection.push(proof1[i]);
-                    break;
-                }
-
-        //no interesection
-        if (intersection.length === 0) return false;
-        let b = intersection[intersection.length-1];
+        let b = this.LCA(proofs1, proofs2);
 
         //best-argm(πA, b) ≥ best-argm(πB, b)
-        return bestArg(proof1, b) >= bestArg(proof2, b);
+        return bestArg(proofs1, b) >= bestArg(proofs2, b);
 
     }
 
