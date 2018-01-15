e841f2aa8427a1a320f95e2ae14fae088d08f651
alexandru.panait <ionutalexpanait@gmail.com>
2018-01-15 22:18:04 +0200

    add multisign

--- src/common/blockchain/interface-blockchain/addresses/Interface-Blockchain-Address-Helper2.js
+++ src/common/blockchain/interface-blockchain/addresses/Interface-Blockchain-Address-Helper2.js
@@ -76,6 +76,27 @@ class InterfaceBlockchainAddressHelper2 {
         })
     }
 
+    makeMultisigAddress() {
+
+        var privKeys = [ bitcoin.ECKey.makeRandom(),
+            bitcoin.ECKey.makeRandom(),
+            bitcoin.ECKey.makeRandom() ];
+
+        var pubKeys = privKeys.map(function(x) { return x.pub });
+
+        var redeemScript = bitcoin.scripts.multisigOutput(2, pubKeys);
+        var scriptPubKey = bitcoin.scripts.scriptHashOutput(redeemScript.getHash());
+        var address = bitcoin.Address.fromOutputScript(scriptPubKey).toString();
+
+        var o = {};
+        o.address = address;
+        o.redeemScript = redeemScript.toHex();
+        o.privateKeys = privKeys.map(function(x) { return x.toWIF() });
+
+        return o;
+
+    }
+
 }
 
 module.exports = InterfaceBlockchainAddressHelper2;
