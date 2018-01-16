72940da12f2781374c38fab0b7942d6c6e70ccae
Silviu Bogdan Stroe <silviu@silviu-s.com>
2018-01-16 11:58:34 +0200

    travis sudo

--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-sudo apt-get install gcc
 sudo apt-get install clang
 npm install -g node-gyp
+env CXX=g++-5 npm install
+env CXX=g++-5 npm install argon2
--- src/common/blockchain/interface-blockchain/addresses/MultiSig.js
+++ src/common/blockchain/interface-blockchain/addresses/MultiSig.js
@@ -84,6 +84,9 @@ class MultiSig {
         })
     }
 
+
+
+
     static createPrivateKey(dates){
 
         let concatDates = "";
