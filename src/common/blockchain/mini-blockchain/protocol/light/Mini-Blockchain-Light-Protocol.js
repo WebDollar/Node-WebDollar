d8e543cfefb84fb62fcce19e52dec2ce23e74fbc
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-01-31 14:07:08 +0200

    Light Protocol fix for test net

--- src/common/blockchain/mini-blockchain/protocol/light/Mini-Blockchain-Light-Protocol.js
+++ src/common/blockchain/mini-blockchain/protocol/light/Mini-Blockchain-Light-Protocol.js
@@ -1,9 +1,14 @@
 import MiniBlockchainProtocol from "../Mini-Blockchain-Protocol";
+import MiniBlockchainProtocolForkSolver from "./Mini-Blockchain-Light-Protocol-Fork-Solver"
 
 class MiniBlockchainLightProtocol extends MiniBlockchainProtocol{
 
     constructor(blockchain){
         super(blockchain)
+    }
+
+    createForkSolver(){
+        this.forkSolver = new MiniBlockchainProtocolForkSolver(this.blockchain, this);
     }
 
 }
