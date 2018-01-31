fe21594a78db6865363d1ef3ce28e55db5afa080
Cosmin-Dumitru Oprea <opreacosmind@gmail.com>
2018-01-31 17:56:41 +0200

    reward: updates
    Signed-off-by: Cosmin-Dumitru Oprea <opreacosmind@gmail.com>

--- src/tests/utils/reward-simulator/RewardSimulator.test.js
+++ src/tests/utils/reward-simulator/RewardSimulator.test.js
@@ -6,16 +6,17 @@ describe('RewardSimulator', () => {
         
         let T = 15; //Number of seconds per block
         let Y = 100; //Number of total mining years
-        let TS = 50505050505; //Total Supply WEBDs
+        let TS = 100000000000; //Total Supply WEBDs
         let CP = 4; //Cycle period in years
         let N = Y / CP; //Number of cycles
         let MAX_REWARD = TS;
         let BPC = (3600 / T) * 24 * 365 * CP; //blocks per cycle
         let X; // The reward per cycle: X + X/2 + X/4 + X/8 + ... + X/2^(N-1) === TS
         
+        let sum = 0.0;
         //first mining cycle mines >= half of total supply
         for (X = TS/2; X < MAX_REWARD; ++X) {
-            let sum = 0.0;
+            sum = 0.0;
             for (let i = 0; i < N; ++i)
                 sum += X / (1 << i);
             console.log("sum =", sum);
@@ -23,21 +24,24 @@ describe('RewardSimulator', () => {
                 break;
         }
         
-        let FIRST_REWARD = X / BPC;
+        let SR = X / BPC;
         
-        console.log("Total reward per first cycle is", X);
-        console.log("Number of blocks mined per cycle is", BPC);
-        console.log("First block's reward is", FIRST_REWARD);
+        console.log("Total supply:", sum);
+        //console.log("Total reward per first cycle:", X);
+        //console.log("Number of blocks mined per cycle:", BPC);
+        console.log("Starting reward:", SR);
+        console.log("Total supply for Y years, CP year cycle, T seconds per block, SR starting reward: ~SR*(3600/T)*24*365*4*2");
+        console.log("Simplified formula and more accurate(using 1.9999999403953552 instead of 2): 252288000 * SR / T");
         
-        console.log("Block reward function is: ");
-        
-        FIRST_REWARD = 5500;
+        SR = 2500;
         let TOTALSUPPLY = 0.0;
         for (let i = 0; i < N; ++i){
-            TOTALSUPPLY += (FIRST_REWARD * BPC) / (1 << i);
+            TOTALSUPPLY += (SR * BPC) / (1 << i);
         }
-        
+
         console.log("Total supply is", TOTALSUPPLY);
+        
+        console.log("Total supply is", SR*(3600/T)*24*365*4*1.9999999403953552);
     });
     
     it('reward simulator test - particular formula', ()=>{
