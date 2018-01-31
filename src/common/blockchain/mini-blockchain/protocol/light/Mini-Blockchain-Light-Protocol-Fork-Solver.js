3351ff436499ed9a2afe0d21422a2f74d78f271c
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-01-31 14:18:05 +0200

    quick fix for deploy #1

--- src/common/blockchain/mini-blockchain/protocol/light/Mini-Blockchain-Light-Protocol-Fork-Solver.js
+++ src/common/blockchain/mini-blockchain/protocol/light/Mini-Blockchain-Light-Protocol-Fork-Solver.js
@@ -1,9 +1,10 @@
 import InterfaceBlockchainProtocolForkSolver from 'common/blockchain/interface-blockchain/protocol/Interface-Blockchain-Protocol-Fork-Solver'
 import PPowBlockchainProtocolForkSolver from 'common/blockchain/ppow-blockchain/protocol/PPoW-Blockchain-Protocol-Fork-Solver'
+import consts from 'consts/const_global'
 
 let inheritForkSolver;
-if (consts.POPOW_ACTIVATED) inheritProtocol = PPowBlockchainProtocolForkSolver;
-else inheritProtocol = InterfaceBlockchainProtocolForkSolver;
+if (consts.POPOW_ACTIVATED) inheritForkSolver = PPowBlockchainProtocolForkSolver;
+else inheritForkSolver = InterfaceBlockchainProtocolForkSolver;
 
 
 class MiniBlockchainLightProtocolForkSolver extends inheritForkSolver{
