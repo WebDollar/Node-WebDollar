d9f4b919f8ef3047f3fef6a6eac1c1a3d3c7ff4c
alexandru.panait <ionutalexpanait@gmail.com>
2018-02-04 00:21:05 +0200

    pool reward function & unit tests

--- src/common/poolMining/PoolLeaderProtocol.js
+++ src/common/poolMining/PoolLeaderProtocol.js
@@ -1,8 +1,9 @@
 import NodesList from 'node/lists/nodes-list';
 import InterfaceSatoshminDB from 'common/satoshmindb/Interface-SatoshminDB'
-import BlockchainMiningReward from 'common/blockchain/global/Blockchain-Mining-Reward';
+// import BlockchainMiningReward from 'common/blockchain/global/Blockchain-Mining-Reward';
 
 const BigNumber = require('bignumber.js');
+var BigInteger = require('big-integer');
 
 class PoolLeaderProtocol {
 
@@ -20,7 +21,7 @@ class PoolLeaderProtocol {
         else
             this.db = dataBase;
 
-        this.blockchainReward = BlockchainMiningReward.getReward();
+        // this.blockchainReward = BlockchainMiningReward.getReward();
         this.hashTarget = new Buffer("00978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb", "hex"); //target difficulty;
 
     }
@@ -62,24 +63,6 @@ class PoolLeaderProtocol {
     createMinerTask() {
 
         //To create minner task puzzle
-
-    }
-
-    getHigherHashDifficulty(hashList) {
-
-        let higherHash = new Buffer("00978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb", "hex"); //target difficulty;
-
-        for (let i = 0; i < hashList.length; ++i) {
-
-            if (hashList[i].compare(higherHash) <= 0) {
-
-                higherHash = hashList[i];
-
-            }
-
-        }
-
-        return higherHash;
 
     }
 
@@ -134,6 +117,7 @@ class PoolLeaderProtocol {
     //          hashList.address (minner identifier address)
     //          hashList.hash (minner best hash)
     //          hashList.reward (will be filled by this function)
+    //          hashList.difficulty (difficulty of hash)
     //
     //  Return:
     //  - hashList
@@ -143,37 +127,35 @@ class PoolLeaderProtocol {
         // Convert to bignumber
         reward = new BigNumber(reward);
 
-        let poolLeaderReward = new BigNumber(reward.dividedBy(100)).multiply(poolLeaderCommission);
+        let poolLeaderReward = new BigNumber(reward).dividedBy(100).mul(poolLeaderCommission);
+
         let minnersReward =  new BigNumber(reward).minus(poolLeaderReward);
 
         // Create hash difficulties list from all minners best hasses
-        let hashDifficultiesList = this.generateHashDificulties(this.hashTarget,hashList);
+        hashList = this.generateHashDificulties(this.hashTarget,hashList);
 
         // Calculate total of Difficulties list
         let totalDifficulties =  new BigNumber(0);
-        for (let i=0; i<hashDifficultiesList.length; i++){
+        for (let i=0; i<hashList.length; i++){
 
-            totalDifficulties = totalDifficulties.plus(hashDifficultiesList[i]);
+            totalDifficulties = totalDifficulties.plus(hashList[i].difficulty);
 
         }
 
         // Add to hashList rewards for each minner
-        for (let i=0; i<hashDifficultiesList.length; i++){
+        for (let i=0; i<hashList.length; i++){
 
-            let currentDifficultyPercent = Math.floor((hashDifficultiesList[i] / totalDifficulties) * 100);
-            currentDifficultyPercent = currentDifficultyPercent.multiply(100);
+            let currentDifficultyPercent = new BigNumber (hashList[i].difficulty).dividedBy(totalDifficulties).mul(100);
 
-            let currentMinerReward = new BigNumber(currentDifficultyPercent).dividedBy(100);
-            currentMinerReward = currentMinerReward.multiply(minnersReward);
-            hashList[i].reward = currentMinerReward;
+            let currentMinerReward = new BigNumber(currentDifficultyPercent).dividedBy(100).mul(minnersReward);
 
-            minnersReward = minnersReward.minus(currentMinerReward);
+            hashList[i].reward = currentMinerReward.toString();
 
         }
 
         return {
-            poolLeader: poolLeaderReward,
-            minners: hashList
+            poolLeaderReward: poolLeaderReward,
+            minnersReward: hashList
         }
 
     }
@@ -192,4 +174,4 @@ class PoolLeaderProtocol {
 
 }
 
-export default new PoolLeaderProtocol();
+export default PoolLeaderProtocol;
---
+++ src/tests/poolMining/PoolLeaderProtocol.test.js
@@ -0,0 +1,60 @@
+
+var assert = require('assert');
+const BigNumber = require('bignumber.js');
+
+import PoolLeaderProtocol from 'common/poolMining/PoolLeaderProtocol.js';
+
+
+describe('test pool leader protocol', () => {
+
+    let miner = new PoolLeaderProtocol();
+
+    let targetHash = new Buffer("00978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb", "hex");
+    let hashList = [
+        {
+            address:"ad1",
+            hash:"00078112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb",
+            reward:0,
+            difficulty:0
+        },
+        {
+            address:"ad1",
+            hash:"00008112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb",
+            reward:0,
+            difficulty:0
+        },
+        {
+            address:"ad1",
+            hash:"00000112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb",
+            reward:0,
+            difficulty:0
+        },
+    ];
+
+    it('generate hash dificulties', ()=>{
+
+        let respose = miner.generateHashDificulties(targetHash,hashList);
+
+        assert(respose[0].difficulty>respose[1].difficulty>respose[2].difficulty,"Correct");
+
+    });
+
+    it('create reward distribution', ()=>{
+
+        let reward = 2500;
+        let leaderCommissionPercentage = 10;
+        let respose = miner.rewardsDistribution(reward,leaderCommissionPercentage,hashList);
+
+        assert(respose.poolLeaderReward==reward/leaderCommissionPercentage,"Good Pool leader reward");
+
+        let reward0 = new BigNumber(respose.minnersReward[0].reward);
+        let reward1 = new BigNumber(respose.minnersReward[1].reward);
+        let reward2 = new BigNumber(respose.minnersReward[2].reward);
+        let totalMinersReward = reward0.plus(reward1).plus(reward2);
+
+        assert(totalMinersReward==reward-respose.poolLeaderReward,"Good minners reward distribution with "+totalMinersReward+ " WEBD");
+
+    });
+
+});
+
