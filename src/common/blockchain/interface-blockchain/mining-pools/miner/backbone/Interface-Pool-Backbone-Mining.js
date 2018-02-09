c5511061670e018d64ec3f271b649911b33f5f8e
Cosmin-Dumitru Oprea <opreacosmind@gmail.com>
2018-02-10 00:52:47 +0200

    pool-mining: Added workers list
    Signed-off-by: Cosmin-Dumitru Oprea <opreacosmind@gmail.com>

--- src/common/blockchain/interface-blockchain/mining-pools/miner/backbone/Interface-Pool-Backbone-Mining.js
+++ src/common/blockchain/interface-blockchain/mining-pools/miner/backbone/Interface-Pool-Backbone-Mining.js
@@ -7,6 +7,23 @@ class InterfacePoolBackboneMining extends InterfaceBlockchainBackboneMining {
         super();
 
     }
+
+    mine(difficultyTarget){
+
+        this.difficulty = difficultyTarget;
+
+        let promiseResolve = new Promise ( (resolve) => {
+
+
+            this._workerResolve = resolve;
+            setTimeout(async () => {return await this.mineNonces() }, 10);
+
+
+        } );
+
+        return promiseResolve;
+
+    }
 }
 
 export default InterfacePoolBackboneMining;
---
+++ src/common/blockchain/interface-blockchain/mining-pools/miner/browser/Interface-Pool-Browser-Workers-List.js
@@ -0,0 +1,12 @@
+import InterfaceBlockchainMiningWorkersList from "common/blockchain/interface-blockchain/mining/Interface-Blockchain-Mining-Workers-List";
+
+class InterfacePoolBrowserWorkersList extends InterfaceBlockchainMiningWorkersList {
+
+    constructor(mining) {
+
+        super(mining);
+
+    }
+}
+
+export default InterfacePoolBrowserWorkersList;
