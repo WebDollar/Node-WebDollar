35af8ff3f232bc007738644ff2e7258600d8d82d
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-02-12 17:24:32 +0200

    fix

--- src/common/sockets/protocol/node-protocol.js
+++ src/common/sockets/protocol/node-protocol.js
@@ -29,7 +29,7 @@ class NodeProtocol {
 
             if (response.version < consts.NODE_VERSION_COMPATIBILITY){
 
-                console.log(colors.red("hello received, VERSION is not right"), response.version);
+                console.log(colors.red("hello received, VERSION is not right"), response.version, consts.NODE_VERSION_COMPATIBILITY);
                 return false;
 
             }
