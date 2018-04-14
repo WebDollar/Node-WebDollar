2cea01dc17e16c8a07ecc431d6861e56dad09bc0
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-04-14 13:26:08 +0300

    satoshi fix

--- src/consts/const_global.js
+++ src/consts/const_global.js
@@ -135,8 +135,8 @@ consts.ADDRESSES.ADDRESS.WIF.LENGTH = consts.ADDRESSES.ADDRESS.LENGTH + consts.A
 
 
 consts.HASH_ARGON2_PARAMS = {
-    salt: 'WebDollar_make_$',
-    saltBuffer: Buffer.from("WebDollar_make_$"),
+    salt: 'Satoshi_is_Finney',
+    saltBuffer: Buffer.from("Satoshi_is_Finney"),
     time: 2,
     memPower:10,
     memBytes: 1024,
