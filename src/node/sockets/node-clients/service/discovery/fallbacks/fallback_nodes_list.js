a411ae4e9a804bf9ddaf30b55483f90fbb004279
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-04-16 15:03:26 +0300

    update

--- src/node/sockets/node-clients/service/discovery/fallbacks/fallback_nodes_list.js
+++ src/node/sockets/node-clients/service/discovery/fallbacks/fallback_nodes_list.js
@@ -8,14 +8,6 @@ export default {
         {
             "addr": ["webdollar.ddns.net:80", "webdollar.ddns.net:8081"],
         },
-        {
-            "addr": ["skyhub.me", "92.222.85.90"],
-            "port": 80,
-        },
-        {
-            "addr": ["presa7.ro"],
-            "port": 443,
-        },
 
     ]
 }
