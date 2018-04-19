8346f6ecc245bb0ab421c05ceba2cf73aa35a17d
Cosmin-Dumitru Oprea <opreacosmind@gmail.com>
2018-04-19 21:24:22 +0300

    threads: Completed ThreadPool.addTask function
    Signed-off-by: Cosmin-Dumitru Oprea <opreacosmind@gmail.com>

--- src/common/threads/ThreadPool.js
+++ src/common/threads/ThreadPool.js
@@ -2,50 +2,49 @@ const Pool = require('threads').Pool;
 
 class ThreadPool {
 
-    constructor(numThreads = 1) {
+    constructor(numThreads = 1){
         this.pool = new Pool(numThreads);
-        this._runDemo();
-    }
-
-    addWorker() {
-
-    }
-
-    removeWorker() {
-
-    }
-
-    addTask(taskCallback, inputData) {
-       this.pool.run(taskCallback).send(inputData);
-    }
-
-    _runDemo() {
-
-//        this.pool.run('src/common/threads/Worker.js').send({ do : 'something' });
+        
         this.pool
-        .on('done', function(job, message) {
+        .on('done', (job, message) => {
             console.log('Job done:', message);
-            console.log();
         })
-        .on('error', function(job, error) {
+        .on('error', (job, error) => {
             console.error('Job errored:', error);
         })
-        .on('finished', function() {
+        .on('finished', () => {
             console.log('Thread pool finished properly!!!.');
             this.pool.killAll();
         });
     }
+
+    addWorker(){
+
+    }
+
+    removeWorker(){
+
+    }
+    
+    setWorkers(value){
+
+    }
+
+    addTask(taskCallback, inputData) {
+        this.pool.run(taskCallback).send(inputData);
+    }
+
 }
 
-function callback(input) {
+function callback(input, done) {
     console.log("Callback: ", input);
+    done("Task had been finished");
 }
 
 let TP = new ThreadPool(4);
-TP.addTask(callback, "cosmin");
-TP.addTask(callback, "dumitru");
-TP.addTask(callback, "oprea");
 
-console.log();
+for (let i = 0; i < 100; ++i){
+    TP.addTask(callback, i);
+}
 
-export default ThreadPool;
+//export default ThreadPool;
