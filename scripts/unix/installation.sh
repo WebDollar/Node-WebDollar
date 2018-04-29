e5c68041adbc4189059181241c3fcc0510bea19e
ionutAlexandruPanait <ionutalexpanait@gmail.com>
2018-04-30 02:57:39 +0300

    instance install script

--- scripts/unix/installation.sh
+++ scripts/unix/installation.sh
@@ -1,29 +1,39 @@
-sudo apt-get install git ;
-mkdir /home/WebDollar ;
-git clone https://github.com/WebDollar/Node-WebDollar.git Node-WebDollar ;
+sudo apt-get install git
+mkdir /home/WebDollar
+git clone https://github.com/WebDollar/Node-WebDollar.git Node-WebDollar
 
-sudo apt-get update ;
+cd ./Node-WebDollar
+yes Y | command-that-asks-for-input
 
-dpkg --add-architecture i386 ;
-apt-get update ;
-apt-get install libssl-dev:i386 ;
+sudo apt-get update
 
-sudo apt-get install build-essential libssl-devstart ;
+sudo apt-get install build-essential libssl-devstart
 
+mkdir nvm
+sudo apt-get install curl
+curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh
 
-mkdir nvm ;
-sudo apt-get install curl ;
-curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh ;
+bash install_nvm.sh
+source ~/.profile
 
+nvm ls-remote
+nvm install 8.2.1
+nvm use 8.2.1
+node -v
 
-bash install_nvm.sh ;
-source ~/.profile ;
+nvm ls
+nvm alias default 8.2.1
+nvm use default
 
-nvm ls-remote ;
-nvm install 8.2.1 ;
-nvm use 8.2.1;
-node -v ;
+npm install
 
-nvm ls ;
-nvm alias default 8.2.1 ;
-nvm use default ;
+sudo apt install linuxbrew-wrapper
+sudo apt-get install clang
+npm install -g node-gyp
+
+env CXX=g++-5 npm install
+env CXX=g++-5 npm install argon2
+
+npm install pm2 -g
+
+./start.sh
