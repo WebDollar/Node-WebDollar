77c911b61cc63216a0d948dc6013fef26e3c0b49
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-04-30 19:20:44 +0300

    Signaling Server

--- src/main-blockchain/Blockchain.js
+++ src/main-blockchain/Blockchain.js
@@ -1,7 +1,6 @@
 import MainBlockchainWallet from 'main-blockchain/wallet/Main-Blockchain-Wallet';
 import MainBlockchain from 'main-blockchain/chain/Main-Blockchain';
 import MainBlockchainMining from 'main-blockchain/mining/Main-Blockchain-Mining';
-import MainBlockchainProtocol from 'main-blockchain/blockchain-protocol/Main-Blockchain-Protocol';
 import MainBlockchainAgent from 'main-blockchain/agents/Main-Blockchain-Agent';
 import MainBlockchainBalances from "main-blockchain/balances/Main-Blockchain-Balances";
 import ValidationsUtils from "common/utils/validation/Validations-Utils";
