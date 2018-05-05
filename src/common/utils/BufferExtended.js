c63029fac4a4cc78e1d55932d4cad1a1bb967b17
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-05-05 03:46:47 +0300

    Buffer Extended fix

--- src/common/utils/BufferExtended.js
+++ src/common/utils/BufferExtended.js
@@ -13,8 +13,10 @@ class BufferExtended {
 
         let length = Math.min(index + count, buffer.length);
 
-        if (length-index <= 0)
+        if (length-index < 0)
             throw {message: "length-index <= 0...", buffer: buffer.toString("hex"), index:index, length:length, count: count};
+
+        if (length-index === 0) return new Buffer(0);
 
         let buf = new Buffer(length-index);
         buffer.copy(buf, 0, index, length);
