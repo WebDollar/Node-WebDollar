36dfa094c5c6979c4c6534f461143c3aed48dc20
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-05-07 20:37:45 +0300

    minor update

--- src/node/sockets/node-clients/service/discovery/fallbacks/fallback_nodes_list.js
+++ src/node/sockets/node-clients/service/discovery/fallbacks/fallback_nodes_list.js
@@ -13,20 +13,15 @@ export default {
         {"addr": ["webdollar.ddns.net:8083"],},
 
         { "addr": ["skyhub.me:80"], },
-        { "addr": ["skyhub.me:443"], },
 
         {"addr": ["presa7.ro:80"],},
-        {"addr": ["presa7.ro:443"],},
 
         {"addr": ["webdollar-vps1.ddns.net:80"],},
-        {"addr": ["webdollar-vps1.ddns.net:443"],},
 
         {"addr": ["webdollar-vps2.ddns.net:80"],},
-        {"addr": ["webdollar-vps2.ddns.net:443"],},
 
 
         {"addr": ["webdollar-vps3.ddns.net:80"],},
-        {"addr": ["webdollar-vps3.ddns.net:443"],},
 
 
         { "addr": ["hcern.ddns.net:80"]},
