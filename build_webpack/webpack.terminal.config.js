43473d2887021abd9dc0e5a3cb6053a5798c0877
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-05-11 00:52:26 +0300

    start.sh

--- build_webpack/webpack.terminal.config.js
+++ build_webpack/webpack.terminal.config.js
@@ -30,6 +30,7 @@ const config = merge(base, {
                 SERVER_PORT: process.env.SERVER_PORT,
                 INSTANCE_PREFIX: process.env.INSTANCE_PREFIX,
                 MAXIMUM_CONNECTIONS_FROM_BROWSER: process.env.MAXIMUM_CONNECTIONS_FROM_BROWSER,
+                MAXIMUM_CONNECTIONS_FROM_TERMINAL: process.env.MAXIMUM_CONNECTIONS_FROM_TERMINAL,
             }
         })
     ]
