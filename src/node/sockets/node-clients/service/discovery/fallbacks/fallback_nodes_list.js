96e9347bbbd992517559917c5f480fb62375feea
ionutAlexandruPanait <ionutalexpanait@gmail.com>
2018-05-18 14:41:30 +0300

    fallback list update

--- src/node/sockets/node-clients/service/discovery/fallbacks/fallback_nodes_list.js
+++ src/node/sockets/node-clients/service/discovery/fallbacks/fallback_nodes_list.js
@@ -5,39 +5,46 @@ export default {
 
     "nodes": [
 
+        //---------------------------------------------------------
+        //--------------Community FallBack Nodes-------------------
+        //---------------------------------------------------------
 
-        {"addr": ["webdollar.ddns.net:80"],},
-        {"addr": ["webdollar.ddns.net:8080"], },
-        {"addr": ["webdollar.ddns.net:8081"], },
-        {"addr": ["webdollar.ddns.net:8082"], },
+        {"addr": ["https://webdollar.bitcoinplusplus.com:443"]},
+        {"addr": ["https://amsterdam.wdpool.io:443"]},
+        {"addr": ["https://strasbourg.wdpool.io:443"]},
+        {"addr": ["https://paris.wdpool.io:443"]},
+        {"addr": ["https://webdollar.csland.ro:443"]},
+        {"addr": ["https://nodecstl.ddns.net:80"]},
+        {"addr": ["https://webd.5q.ro:3333"]},
+        {"addr": ["https://shpool.ml:443"]}, // Thanks to @Amahte
+        {"addr": ["https://webdollar.network:5000"]}, // Thanks to @ader1990
 
-        {"addr": ["webdollar.network:5000"]},
-        {"addr": ["webd.5q.ro:3333"]},
-        {"addr": ["webdollar.bitcoinplusplus.com:443"]},
-        {"addr": ["amsterdam.wdpool.io:443"]},
+        {"addr": ["https://robitza.ddns.net:80"]}, // Thanks to @robertclaudiu
+        {"addr": ["https://robitza.ddns.net:443"]}, // Thanks to @robertclaudiu
+        {"addr": ["https://robitza.ddns.net:8080"]}, // Thanks to @robertclaudiu
+        {"addr": ["https://robitza.ddns.net:8081"]}, // Thanks to @robertclaudiu
+        {"addr": ["https://robitza.ddns.net:8082"]}, // Thanks to @robertclaudiu
 
-        {"addr": ["skyhub.me:80"], },
+        //---------------------------------------------------------
+        //--------------WebDollar FallBack Nodes-------------------
+        //---------------------------------------------------------
 
-        {"addr": ["presa7.ro:80"], },
+        {"addr": ["https://webdollar.ddns.net:80"]},
+        {"addr": ["https://webdollar.ddns.net:8080"]},
+        {"addr": ["https://webdollar.ddns.net:8081"]},
+        {"addr": ["https://webdollar.ddns.net:8082"]},
 
-        {"addr": ["webdollar-vps1.ddns.net:80"],},
+        {"addr": ["https://skyhub.me:80"]},
+        {"addr": ["https://presa7.ro:80"]},
 
-        {"addr": ["webdollar-vps2.ddns.net:80"],},
+        {"addr": ["https://webdollar-vps1.ddns.net:80"]},
+        {"addr": ["https://webdollar-vps2.ddns.net:80"]},
+        {"addr": ["https://webdollar-vps3.ddns.net:80"]},
 
-        {"addr": ["webdollar-vps3.ddns.net:80"],},
-
-        {"addr": ["hcern.ddns.net:80"]},
-        {"addr": ["hcern.ddns.net:443"]},
-        {"addr": ["hcern.ddns.net:8080"]},
-        {"addr": ["hcern.ddns.net:8081"]},
-
-        {"addr": ["robitza.ddns.net:80"]},
-        {"addr": ["robitza.ddns.net:443"]},
-        {"addr": ["robitza.ddns.net:8080"]},
-        {"addr": ["robitza.ddns.net:8081"]},
-        {"addr": ["robitza.ddns.net:8082"]},
-
-
+        {"addr": ["https://hcern.ddns.net:80"]},
+        {"addr": ["https://hcern.ddns.net:443"]},
+        {"addr": ["https://hcern.ddns.net:8080"]},
+        {"addr": ["https://hcern.ddns.net:8081"]},
 
     ]
 }
