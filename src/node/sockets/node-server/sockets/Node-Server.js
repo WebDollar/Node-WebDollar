d696da20d9f5ad94fb21fa282e3c884660fefe15
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-05-19 01:47:57 +0300

    Node Server update

--- src/node/sockets/node-server/sockets/Node-Server.js
+++ src/node/sockets/node-server/sockets/Node-Server.js
@@ -17,8 +17,8 @@ import AGENT_STATUS from "common/blockchain/interface-blockchain/agents/Agent-St
 const TIME_DISCONNECT_TERMINAL = 15*60*1000;
 const TIME_DISCONNECT_TERMINAL_TOO_OLD_BLOCKS = 5*60*1000;
 
-const TIME_TO_PASS_TO_CONNECT_NEW_CLIENT = 5*1000;
-const SERVER_FREE_ROOM = 10;
+const TIME_TO_PASS_TO_CONNECT_NEW_CLIENT = 4*1000;
+const SERVER_FREE_ROOM = 20;
 
 class NodeServer {
 
