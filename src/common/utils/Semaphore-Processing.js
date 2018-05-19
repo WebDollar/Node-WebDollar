44b4ad069ba1f2d2a069f2093dce8e1b03f071f9
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-05-19 09:21:52 +0300

    Revert fix

--- src/common/utils/Semaphore-Processing.js
+++ src/common/utils/Semaphore-Processing.js
@@ -27,13 +27,13 @@ class SemaphoreProcessing{
     }
 
 
-    _processingSemaphoreList(){
+    async _processingSemaphoreList(){
 
         if (this._list.length > 0){
 
             let answer = false;
             try {
-                answer = this._list[0].callback();
+                answer = await this._list[0].callback();
             } catch (ex){
                 console.error("error processingSemaphoreList !!!!!!!!!!!!!!!!!!!!!!!!", ex);
             }
@@ -50,7 +50,7 @@ class SemaphoreProcessing{
 
         }
 
-        setTimeout(this._processingSemaphoreList.bind(this), this.processingSemaphoreInterval);
+        setTimeout( async ()=>{ await this._processingSemaphoreList.bind(this)}, this.processingSemaphoreInterval);
 
     }
 
