4ac927a3c1315b8d1eff82459f5d3374974c7059
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-05-20 07:16:00 +0300

    new const global

--- src/consts/const_global.js
+++ src/consts/const_global.js
@@ -246,7 +246,7 @@ consts.SETTINGS = {
             TERMINAL:{
 
                 CLIENT: {
-                    MAXIMUM_CONNECTIONS_IN_TERMINAL_WAITLIST: 40,
+                    MAXIMUM_CONNECTIONS_IN_TERMINAL_WAITLIST: 10,
                     MAXIMUM_CONNECTIONS_IN_TERMINAL_WAITLIST_FALLBACK: 2,
 
                     SSL:{
@@ -256,8 +256,8 @@ consts.SETTINGS = {
                 },
 
                 SERVER: {
-                    MAXIMUM_CONNECTIONS_FROM_BROWSER: 450,
-                    MAXIMUM_CONNECTIONS_FROM_TERMINAL: 120,
+                    MAXIMUM_CONNECTIONS_FROM_BROWSER: 430,
+                    MAXIMUM_CONNECTIONS_FROM_TERMINAL: 100,
 
                     TERMINAL_CONNECTIONS_REQUIRED_TO_DISCONNECT_FROM_FALLBACK: 10,
                 },
