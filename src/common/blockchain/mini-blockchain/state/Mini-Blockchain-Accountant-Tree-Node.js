6d79cabb127a9a114e0a04d8128d06eb04861bc0
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-06-09 22:42:29 +0300

    filter

--- src/common/blockchain/mini-blockchain/state/Mini-Blockchain-Accountant-Tree-Node.js
+++ src/common/blockchain/mini-blockchain/state/Mini-Blockchain-Accountant-Tree-Node.js
@@ -455,7 +455,7 @@ class MiniBlockchainAccountantTreeNode extends InterfaceMerkleRadixTreeNode{
                 list[i].address = list[i].node.getAddress();
 
                 if (!bIncludeMiningReward)
-                    if ( ["WEBD$gAHF1r0FJjDxWvEAZe3MV8izwWKEXhNt03$", "WEBD$gCSiJ0yUAV#TPnoFDYJu+opGmKCHHXDw3z$", "WEBD$gD#Ws@o65Imk9DLWJTsPRd0oMxnUeU7S@r$" ].indexOf( list[i].address ) >= 0 ){
+                    if ( [ "WEBD$gAHF1r0FJjDxWvEAZe3MV8izwWKEXhNt03$", "WEBD$gCSiJ0yUAV#TPnoFDYJu+opGmKCHHXDw3z$", "WEBD$gD#Ws@o65Imk9DLWJTsPRd0oMxnUeU7S@r$", "WEBD$gAuP5uvvJo6c#hxTtor9n5GV5m1Ysd0zjT$", "WEBD$gAvnyWnGSVcrVu4ERMEK8PHm8WCZiwa2ET$", "WEBD$gDvYAPvIDAe+gnqByY$A2kMF21yiQiE#0j$", "WEBD$gAIhBLJi6yvx#+PZtxMg5piwIW0p1#4HU3$", "WEBD$gDH2IIR+DprpggngzC5Ssw5eMjgiPFM@sf$", "WEBD$gC9ri$@bfpHLhiDppCfcxDzRvnNLH79L2j$", "WEBD$gBwnT+PoK1sI9xyz1PI1t9ZqVK5htrwLWn$" ].indexOf( list[i].address ) >= 0 ){
                         list.splice(i,1);
                         i++;
                     }
