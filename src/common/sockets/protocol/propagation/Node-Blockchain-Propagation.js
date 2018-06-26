743127fa35e8c4ec844eaddadbad2c3ee20681a2
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-06-26 16:37:01 +0300

    avoid propagating blocks to pool miners

--- src/common/sockets/protocol/propagation/Node-Blockchain-Propagation.js
+++ src/common/sockets/protocol/propagation/Node-Blockchain-Propagation.js
@@ -2,6 +2,7 @@ import Blockchain from "main-blockchain/Blockchain"
 import consts from 'consts/const_global'
 import NodesList from 'node/lists/Nodes-List';
 import NodeProtocol from "../extend-socket/Node-Protocol";
+import NODES_CONSENSUS_TYPE from "node/lists/types/Node-Consensus-Type";
 
 class NodeBlockchainPropagation{
 
@@ -44,6 +45,10 @@ class NodeBlockchainPropagation{
             if (! Array.isArray(socketsAvoidBroadcast) )
                 socketsAvoidBroadcast = [socketsAvoidBroadcast];
 
+            for (let i=0; i < NodesList.nodes.length; i++)
+                if ( NodesList.nodes[i].socket.node.protocol.nodeConsensusType !== NODES_CONSENSUS_TYPE.NODE_CONSENSUS_MINER_POOL && NodesList.nodes[i].socket.node.protocol.nodeConsensusType !== NODES_CONSENSUS_TYPE.NODE_CONSENSUS_SERVER_FOR_MINER )
+                    socketsAvoidBroadcast.push(NodesList.nodes[i].socket);
+
             this._socketsAlreadyBroadcast = socketsAvoidBroadcast;
 
         }
@@ -60,8 +65,10 @@ class NodeBlockchainPropagation{
             this._socketsAlreadyBroadcast = [];
         }
 
+        //sending the block, except poolMiners
         for (let i=0; i < NodesList.nodes.length; i++)
-            NodesList.nodes[i].socket.node.protocol.sendLastBlock();
+            if ( NodesList.nodes[i].socket.node.protocol.nodeConsensusType !== NODES_CONSENSUS_TYPE.NODE_CONSENSUS_MINER_POOL && NodesList.nodes[i].socket.node.protocol.nodeConsensusType !== NODES_CONSENSUS_TYPE.NODE_CONSENSUS_SERVER_FOR_MINER )
+                NodesList.nodes[i].socket.node.protocol.sendLastBlock();
 
     }
 
