594245b4583d4415639b3d585644ce3b981833a1
Cristian Antal <xkid@xkid.ro>
2018-06-27 13:20:17 +0300

    updated start script to include pools and pool servers

--- start_docker_letsencrypt.sh
+++ start_docker_letsencrypt.sh
@@ -6,6 +6,9 @@ if [ -z $MAXIMUM_CONNECTIONS_FROM_BROWSER ]; then export MAXIMUM_CONNECTIONS_FRO
 if [ -z $MAXIMUM_CONNECTIONS_FROM_TERMINAL ]; then export MAXIMUM_CONNECTIONS_FROM_TERMINAL=256; fi
 if [ -z $FEE ]; then export FEE=0.18; fi
 if [ -z $TYPE ]; then export TYPE=12; fi
+if [ -z $POOL_NAME ]; then echo 'You need to input a pool name using POOL_NAME ENV variable'; fi
+if [ -z $POOL_WEBSITE ]; then echo 'You need to input a pool website using POOL_WEBSITE ENV variable'; fi
+if [ -z $POOL_SERVERS ]; then export POOL_SERVERS='https://p1.webdollar.fun'; fi
 
 # Check NOSSL in case we don't need a SSL
 if [ -z $NOSSL ]; then
@@ -26,4 +29,8 @@ if [ -z $NOSSL ]; then
 fi
 
 # Start Pool
+if [ $TYPE = 11]; then
+(sleep 15;echo 11;sleep 5;echo $FEE;sleep 5;echo $POOL_NAME;sleep 5;echo $POOL_WEBSITE;sleep 5;echo y;sleep 5;echo $POOL_SERVERS;) | npm run commands
+else
 (sleep 15;echo $TYPE;sleep 5;echo $FEE;) | npm run commands
+fi
