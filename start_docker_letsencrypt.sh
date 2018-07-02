e05014a9fde395e51cc2d240f41a8325a1273b5c
Cristian Antal <xkid@xkid.ro>
2018-07-02 14:35:18 +0300

    fixed if in script

--- start_docker_letsencrypt.sh
+++ start_docker_letsencrypt.sh
@@ -28,7 +28,7 @@ fi
 
 if [ -z $TYPE ]; then
 
-	if [ $TYPE -eq 11 ]; then
+	if [ $TYPE -eq "11" ]; then
 		# Start Pool
 		if [ -z $POOL_NAME ]; then echo 'You need to input a pool name using POOL_NAME ENV variable'; exit; fi
 		if [ -z $POOL_EXTERNAL ]; then echo 'You need to input a pool website using POOL_EXTERNAL ENV variable'; exit; fi
@@ -41,7 +41,7 @@ if [ -z $TYPE ]; then
 			if [ -z $POOL_SERVERS ]; then export POOL_SERVERS='https://p1.webdollar.fun'; fi
 			(sleep 15;echo 11;sleep 5;echo $FEE;sleep 5;echo "$POOL_NAME";sleep 5;echo "$POOL_WEBSITE";sleep 5;echo $POOL_EXTERNAL;sleep 5;echo "$POOL_SERVERS";) | npm run commands
 		fi
-	elif [ $TYPE -eq 12 ]; then
+	elif [ $TYPE -eq "12" ]; then
 		# Start PoolServer
 		(sleep 15;echo $TYPE;sleep 5;echo $FEE;) | npm run commands
 	fi
