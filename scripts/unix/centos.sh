7b55d9ccc569065f62bb12bb39c13c51694e6b8f
Constantin Busuioceanu <39584380+cbusuioceanu@users.noreply.github.com>
2018-07-14 19:56:01 +0300

    Update centos.sh
    updated script for CentOS

--- scripts/unix/centos.sh
+++ scripts/unix/centos.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+
+# Tested on CentOS 7
+
+yum update
 curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
 sudo yum -y install nodejs
-
-sudo yum groupinstall 'Development Tools'
-sudo yum install clang
+sudo yum groupinstall "Development Tools"
+sudo yum -y install clang
 npm install -g node-gyp
-
-env CXX=g++-4 npm install
+npm install
