0612cc2d9dc0a4c36d310f3664b88d12b97596b9
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-07-16 13:20:31 +0300

    fallback nodes

--- src/node/sockets/node-clients/service/discovery/fallbacks/fallback_nodes_list.js
+++ src/node/sockets/node-clients/service/discovery/fallbacks/fallback_nodes_list.js
@@ -100,7 +100,7 @@ export default {
         // {"addr": ["https://webdollar-vps2.ddns.net:80"]},
         // {"addr": ["https://webdollar-vps3.ddns.net:80"]},
 
-
+        {"addr": ["https://webdollar-vps4.zapto.org:80"]},
 
         {"addr": ["https://webdollar-vps5.hopto.org:8080"]},
         {"addr": ["https://webdollar-vps5.hopto.org:8081"]},
@@ -109,7 +109,7 @@ export default {
         {"addr": ["https://webdollar-vps5.hopto.org:8084"]},
 
 
-        {"addr": ["https://webdollar-vps4.zapto.org:80"]},
+
         {"addr": ["https://webdollar-vps4.zapto.org:8081"]},
         {"addr": ["https://webdollar-vps4.zapto.org:8082"]},
         {"addr": ["https://webdollar-vps4.zapto.org:8083"]},
