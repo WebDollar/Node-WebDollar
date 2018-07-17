ea8c9d02aec7a9767cc929eb7079e22c6d0e98ab
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-07-17 20:45:54 +0300

    1.160.5

--- src/consts/const_global.js
+++ src/consts/const_global.js
@@ -234,7 +234,7 @@ consts.SETTINGS = {
 
     NODE: {
 
-        VERSION: "1.160.4",
+        VERSION: "1.160.5",
         VERSION_COMPATIBILITY: "1.160.0",
 
         VERSION_COMPATIBILITY_UPDATE: "",
