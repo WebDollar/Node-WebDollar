14ac60c65c5fd969655ed0f5f59a85807da7b0ce
Ionut Moraru <morion4000@gmail.com>
2018-07-17 17:51:54 +0300

    Update fallback_nodes_list.js

--- src/node/sockets/node-clients/service/discovery/fallbacks/fallback_nodes_list.js
+++ src/node/sockets/node-clients/service/discovery/fallbacks/fallback_nodes_list.js
@@ -56,6 +56,12 @@ export default {
         {"addr": ["https://wd.hoste.ro:40001"]}, // Thanks to @morion4000
         {"addr": ["https://wd.hoste.ro:40002"]}, // Thanks to @morion4000
         {"addr": ["https://wd.hoste.ro:40003"]}, // Thanks to @morion4000
+        {"addr": ["https://wd.hoste.ro:40004"]}, // Thanks to @morion4000
+        {"addr": ["https://wd.hoste.ro:40005"]}, // Thanks to @morion4000
+        {"addr": ["https://wd.hoste.ro:40006"]}, // Thanks to @morion4000
+        {"addr": ["https://wd.hoste.ro:40007"]}, // Thanks to @morion4000
+        {"addr": ["https://wd.hoste.ro:40008"]}, // Thanks to @morion4000
+        {"addr": ["https://wd.hoste.ro:40009"]}, // Thanks to @morion4000
 
         //{"addr": ["https://webdollar.network:5000"]}, // Thanks to @ader1990
 
