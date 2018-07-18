428330e0dfb4f1e24a11809464e66efc174f258f
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-07-18 21:53:01 +0300

    revert optimization

--- src/common/blockchain/interface-blockchain/protocol/Interface-Blockchain-Protocol-Fork-Solver.js
+++ src/common/blockchain/interface-blockchain/protocol/Interface-Blockchain-Protocol-Fork-Solver.js
@@ -154,7 +154,7 @@ class InterfaceBlockchainProtocolForkSolver{
                     if (this.blockchain.blocks[i].hash.equals(answer.hash)){
 
                         binarySearchResult = {
-                            position: i+2,
+                            position: i+1,
                             header: answer.hash,
                         };
 
