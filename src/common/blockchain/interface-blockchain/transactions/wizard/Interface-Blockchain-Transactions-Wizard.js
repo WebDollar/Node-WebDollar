4b57b3da95827d8e4ff7818f9e4cb269920f56d0
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-07-19 18:44:28 +0300

    minimum payout

--- src/common/blockchain/interface-blockchain/transactions/wizard/Interface-Blockchain-Transactions-Wizard.js
+++ src/common/blockchain/interface-blockchain/transactions/wizard/Interface-Blockchain-Transactions-Wizard.js
@@ -171,7 +171,8 @@ class InterfaceBlockchainTransactionsWizard{
         if (webdPerByte === undefined)
             webdPerByte = consts.MINING_POOL.MINING.FEE_PER_BYTE;
 
-        webdPerByte = Math.trunc( serialization.length / 141) + 1;
+        let factor = Math.trunc( serialization.length / 141) + 1;
+        webdPerByte = factor * webdPerByte;
 
         return serialization.length * webdPerByte;
 
