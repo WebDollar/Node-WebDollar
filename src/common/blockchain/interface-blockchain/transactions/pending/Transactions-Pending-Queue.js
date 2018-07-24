65b28bc93afd280e42fc546cf59ca15938887ebb
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-07-24 04:47:12 +0300

    fixing bug with payments

--- src/common/blockchain/interface-blockchain/transactions/pending/Transactions-Pending-Queue.js
+++ src/common/blockchain/interface-blockchain/transactions/pending/Transactions-Pending-Queue.js
@@ -136,12 +136,12 @@ class TransactionsPendingQueue {
 
         for (let i=this.list.length-1; i >= 0; i--) {
 
+            if (this.list[i].from.addresses[0].unencodedAddress.equals( this.blockchain.mining.unencodedMinerAddress )) continue;
+
             try{
 
                 if ( (this.blockchain.blocks.length > this.list[i].pendingDateBlockHeight + consts.SETTINGS.MEM_POOL.TIME_LOCK.TRANSACTIONS_MAX_LIFE_TIME_IN_POOL_AFTER_EXPIRATION ||  !this.list[i].validateTransactionEveryTime(undefined, blockValidationType )) &&
-                     (this.list[i].timeLock === 0 || this.list[i].timeLock < this.blockchain.blocks.length ) &&
-                     !this.list[i].from.addresses[0].unencodedAddress.equals( this.blockchain.mining.unencodedMinerAddress )
-                ) {
+                     (this.list[i].timeLock === 0 || this.list[i].timeLock < this.blockchain.blocks.length )) {
                     this._removePendingTransaction(i);
                 }
 
