4d883e30fcd447cc0eae86f7a930d347ff934b80
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-08-04 03:09:02 +0300

    fixing Light (Browser) chainwork

--- src/common/blockchain/mini-blockchain/protocol/Mini-Blockchain-Advanced-Protocol.js
+++ src/common/blockchain/mini-blockchain/protocol/Mini-Blockchain-Advanced-Protocol.js
@@ -105,7 +105,6 @@ class MiniBlockchainAdvancedProtocol extends MiniBlockchainProtocol{
                     difficultyTarget: difficultyTarget,
                     timeStamp: timestamp,
                     hashPrev: hashPrev,
-                    chainWork: this.blockchain.blocks.chainWorkSerialized
                 });
 
 
