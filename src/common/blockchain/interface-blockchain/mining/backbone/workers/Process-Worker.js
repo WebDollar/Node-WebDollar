a707a993bbba732c57729d630ef0d64f7c5e7410
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-08-05 16:06:47 +0300

    update buffer util version

--- src/common/blockchain/interface-blockchain/mining/backbone/workers/Process-Worker.js
+++ src/common/blockchain/interface-blockchain/mining/backbone/workers/Process-Worker.js
@@ -70,7 +70,7 @@ class ProcessWorker{
 
         });
 
-        await Blockchain.blockchain.sleep(2000);
+        await Blockchain.blockchain.sleep(1500);
 
         if (this._child.exitCode !== null)
             return false;
@@ -86,7 +86,7 @@ class ProcessWorker{
 
         this._prevHash = '';
 
-        await Blockchain.blockchain.sleep(3000);
+        await Blockchain.blockchain.sleep(1500);
 
         if (this._timeoutValidation === undefined)
             this._timeoutValidation = setTimeout(this._validateWork.bind(this), 1000);
