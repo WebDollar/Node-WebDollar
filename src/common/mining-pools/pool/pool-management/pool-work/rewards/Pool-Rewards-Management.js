cbe801cfc56ea84102a3211b9212151871b34a14
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-08-11 09:54:26 +0300

    Pool Payouts solving the issue with Forks & Pool Rewards Management more confirmations

--- src/common/mining-pools/pool/pool-management/pool-work/rewards/Pool-Rewards-Management.js
+++ src/common/mining-pools/pool/pool-management/pool-work/rewards/Pool-Rewards-Management.js
@@ -15,7 +15,7 @@ const CONFIRMATIONS_REQUIRE_OTHER_MINERS = consts.DEBUG ? false : true;
 
 const CONFIRMATION_METHOD = 2; //1 is not working properly
 
-const CONFIRMATIONS_REQUIRED = consts.DEBUG ? 1 : (CONFIRMATION_METHOD === 2 ? 15 : 10);
+const CONFIRMATIONS_REQUIRED = consts.DEBUG ? 1 : (CONFIRMATION_METHOD === 2 ? 60 : 10);
 
 import Blockchain from 'main-blockchain/Blockchain';
 
