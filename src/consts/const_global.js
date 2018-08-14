d5c7fdb0e969e881e2a1b6bc7ffe23ed7e390c4e
IonutAlexandruPanait <ionutalexpanait@gmail.com>
2018-08-14 18:32:56 +0300

    Concurrency downloading - fix

--- src/consts/const_global.js
+++ src/consts/const_global.js
@@ -244,7 +244,7 @@ consts.SETTINGS = {
 
     NODE: {
 
-        VERSION: "1.190",
+        VERSION: "1.191",
 
         VERSION_COMPATIBILITY: "1.174",
         VERSION_COMPATIBILITY_POOL_MINERS: "1.174",
