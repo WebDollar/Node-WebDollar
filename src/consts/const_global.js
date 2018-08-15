ffa7ee315416fa1d2c5a537ddb4c6f19a6c3942b
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-08-15 17:25:39 +0300

    revert Blockchain prover update

--- src/consts/const_global.js
+++ src/consts/const_global.js
@@ -244,7 +244,7 @@ consts.SETTINGS = {
 
     NODE: {
 
-        VERSION: "1.196",
+        VERSION: "1.197",
 
         VERSION_COMPATIBILITY: "1.174",
         VERSION_COMPATIBILITY_POOL_MINERS: "1.174",
