6085ab725e640ee297db15fa9e3bf6c230651b68
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-08-16 22:43:13 +0300

    nipopow version

--- src/consts/const_global.js
+++ src/consts/const_global.js
@@ -246,7 +246,7 @@ consts.SETTINGS = {
 
     NODE: {
 
-        VERSION: "1.197.2",
+        VERSION: "1.197.2NIPOPOW",
 
         VERSION_COMPATIBILITY: "1.174",
         VERSION_COMPATIBILITY_POOL_MINERS: "1.174",
