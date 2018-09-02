b2542d50ea9bd28b166a116e0181e4460f859c44
IonutAlexandruPanait <ionutalexpanait@gmail.com>
2018-09-02 19:58:26 +0300

    last fix

--- src/node/menu/CLI-Menu.js
+++ src/node/menu/CLI-Menu.js
@@ -122,7 +122,7 @@ class CLI {
 
             let addressString = Blockchain.Wallet.addresses[addressId].address;
 
-            Blockchain.blockchain.blocks.length = timelock-1;
+            Blockchain.blockchain.blocks.length = timelock+1;
 
             let answer = await Blockchain.Transactions.wizard.validateTransaction( addressString, toAddress, amountToSend, feeToSend, undefined, undefined, timelock-1, nonce );
             let data ={};
