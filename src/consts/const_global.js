61cf2b93bb21fc29fae5b52b216a66383c630fc1
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-09-06 18:01:52 +0300

    new version to transfer transactions faster in the network

--- src/consts/const_global.js
+++ src/consts/const_global.js
@@ -251,7 +251,7 @@ consts.SETTINGS = {
 
     NODE: {
 
-        VERSION: "1.197.4",
+        VERSION: "1.197.5",
 
         VERSION_COMPATIBILITY: "1.174",
         VERSION_COMPATIBILITY_POOL_MINERS: "1.174",
