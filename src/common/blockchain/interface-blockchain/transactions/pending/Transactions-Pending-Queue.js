bb148d61b859151520a433ad245ff9b4a5377ba3
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-09-12 02:36:02 +0300

    Transactions pending queue bug fix removal

--- src/common/blockchain/interface-blockchain/transactions/pending/Transactions-Pending-Queue.js
+++ src/common/blockchain/interface-blockchain/transactions/pending/Transactions-Pending-Queue.js
@@ -145,7 +145,7 @@ class TransactionsPendingQueue {
             try{
 
                 if ( (this.blockchain.blocks.length > this.list[i].pendingDateBlockHeight + consts.SETTINGS.MEM_POOL.TIME_LOCK.TRANSACTIONS_MAX_LIFE_TIME_IN_POOL_AFTER_EXPIRATION ||  ( Blockchain.blockchain.agent.consensus && !this.list[i].validateTransactionEveryTime(undefined, blockValidationType ))  ) &&
-                     (this.list[i].timeLock === 0 || this.list[i].timeLock < this.blockchain.blocks.length )) {
+                     (this.list[i].timeLock === 0 || this.list[i].timeLock < this.blockchain.blocks.length - consts.SETTINGS.MEM_POOL.TIME_LOCK.TRANSACTIONS_MAX_LIFE_TIME_IN_POOL_AFTER_EXPIRATION  )) {
                     this._removePendingTransaction(i);
                 }
 
