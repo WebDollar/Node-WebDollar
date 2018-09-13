bf4a2a1c99f5bae68a0b56334df08164c5de4c16
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-09-13 21:12:43 +0300

    minor update replacePoolURL

--- src/common/mining-pools/miner/Miner-Pool-Settings.js
+++ src/common/mining-pools/miner/Miner-Pool-Settings.js
@@ -241,8 +241,9 @@ class MinerPoolSettings {
     async _replacePoolURL(url = ''){
 
         if (typeof url !== "string") return url;
-        
-        url = url.replace( "WMP/0.02/bdda9527a040d9063e22e1dccc4d860b84227ff73232f5c418054112114a6ea4/https:$$pool.webdollarminingpool.com:443", "WMP/0.02/c01f57930c27e78e434de1243ae02b98e56d6cd3df42d136be1a1c0a0a9a8624/https:$$server.webdollarminingpool.com:443", );
+
+        if (url.indexOf("WMP/0.02/bdda9527a040d9063e22e1dccc4d860b84227ff73232f5c418054112114a6ea4/https:$$pool.webdollarminingpool.com:443") >= 0)
+            url = url.replace( "WMP/0.02/bdda9527a040d9063e22e1dccc4d860b84227ff73232f5c418054112114a6ea4/https:$$pool.webdollarminingpool.com:443", "WMP/0.02/c01f57930c27e78e434de1243ae02b98e56d6cd3df42d136be1a1c0a0a9a8624/https:$$server.webdollarminingpool.com:443", );
 
         return url;
     }
