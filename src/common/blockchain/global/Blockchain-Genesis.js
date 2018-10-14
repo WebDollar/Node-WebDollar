6071db306512da702c965ee8f3bc263171b79409
IonutAlexandruPanait <ionutalexpanait@gmail.com>
2018-10-14 13:40:03 +0300

    new genesis timestamp

--- src/common/blockchain/global/Blockchain-Genesis.js
+++ src/common/blockchain/global/Blockchain-Genesis.js
@@ -8,7 +8,7 @@ class BlockchainGenesis{
         this.hashPrev = new Buffer("731D46C131EB6DD4667A96BDC27BAF9223BEC72C3468DFB6BA52C460E76423A4", "hex"); //main net
 
         this.timeStamp = 0;
-        this.timeStampOffset = 1524742312; //main net
+        this.timeStampOffset = 1539513579; //main net
         //this.timeStampOffset = 1529344475; //test net
 
         this.difficultyTarget = new Buffer ( "00029112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb", "hex" ); //hard difficulty
