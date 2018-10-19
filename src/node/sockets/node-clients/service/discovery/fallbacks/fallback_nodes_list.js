39999709de3a6e62e8aafc77d7c2e1572156388e
Ionut Moraru <morion4000@gmail.com>
2018-10-19 17:11:23 +0300

    Update fallback_nodes_list.js

--- src/node/sockets/node-clients/service/discovery/fallbacks/fallback_nodes_list.js
+++ src/node/sockets/node-clients/service/discovery/fallbacks/fallback_nodes_list.js
@@ -107,8 +107,8 @@ export default {
     ],
     
     "nodes_testnet": [
-        // {"addr": ["http://testnet1.hoste.ro:8000"]}, // Thanks to Dani Petreus
-        // {"addr": ["http://testnet2.hoste.ro:8001"]}, // Thanks to Dani Petreus
-        // {"addr": ["http://testnet2.hoste.ro:8002"]}, // Thanks to Dani Petreus
+        {"addr": ["http://testnet1.hoste.ro:8000"]}, // Thanks to Hostero
+        {"addr": ["http://testnet2.hoste.ro:8001"]}, // Thanks to Hostero
+        {"addr": ["http://testnet2.hoste.ro:8002"]}, // Thanks to Hostero
     ]
 }
