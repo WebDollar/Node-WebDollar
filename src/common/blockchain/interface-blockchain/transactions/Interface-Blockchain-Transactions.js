ea41dea586ab45dcaa52a311994b8b622db2111e
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2018-10-22 17:30:25 +0300

    fix transactions Proof of Stake mining

--- src/common/blockchain/interface-blockchain/transactions/Interface-Blockchain-Transactions.js
+++ src/common/blockchain/interface-blockchain/transactions/Interface-Blockchain-Transactions.js
@@ -38,6 +38,7 @@ class InterfaceBlockchainTransactions extends InterfaceBlockchainTransactionsEve
         this.wizard.wallet = newWallet;
     }
 
+
 }
 
 export default InterfaceBlockchainTransactions;
