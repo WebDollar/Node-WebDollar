6ddb221e670b186f234c545051b48c011b737dec
Ionut Moraru <morion4000@gmail.com>
2018-11-21 16:21:01 +0200

    Update start_with_wallet.sh

--- start_with_wallet.sh
+++ start_with_wallet.sh
@@ -2,6 +2,11 @@ if [ "$WALLET" = "" ]
 then
   (sleep 60;echo 8;) | npm run commands || true
 else
-  echo $WALLET > wallet.json
-  (sleep 60;echo 4;sleep 5;echo 'wallet.json';sleep 5;echo 3;sleep 5;echo 0;sleep 5;echo 'y';sleep 5;echo 8;) | npm run commands || true
+  if [ -f "wallet.json" ]
+  then
+    (sleep 60;echo 8;) | npm run commands || true
+  else
+    echo $WALLET > wallet.json
+    (sleep 60;echo 4;sleep 5;echo 'wallet.json';sleep 5;echo 3;sleep 5;echo 0;sleep 5;echo 'y';sleep 5;echo 8;) | npm run commands || true
+  fi
 fi
