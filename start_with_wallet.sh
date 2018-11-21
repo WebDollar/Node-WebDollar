8890060b0c22abf2f68723a63e8e262ba63ec6a9
IonutAlexandruPanait <ionutalexpanait@gmail.com>
2018-11-21 20:15:58 +0200

    work around

--- start_with_wallet.sh
+++ start_with_wallet.sh
@@ -3,6 +3,6 @@ then
   (sleep 60;echo 8;) | npm run commands || true
 else
   echo $WALLET > wallet.json
-  (sleep 20;echo 3; sleep 5; echo 0; sleep 5; echo 'y';sleep 5;echo 4;sleep 5;echo 'wallet.json';exit;) | npm run commands || true
+  (sleep 20;echo 3; sleep 5; echo 0; sleep 5; echo 'y';sleep 5;echo 4;sleep 5;echo 'wallet.json';sleep 5;exit;) | npm run commands || true
   (sleep 20;echo 8;) | npm run commands || true
 fi
