c13c926604f8f63cc137abe9ff9c9e7b45fa8dfa
IonutAlexandruPanait <ionutalexpanait@gmail.com>
2018-11-22 15:17:25 +0200

    fix script

--- start_with_wallet.sh
+++ start_with_wallet.sh
@@ -2,7 +2,7 @@ if [ "$WALLET" = "" ]
 then
   (sleep 60;echo 8;) | npm run commands || true
 else
-  echo $WALLET > wallet.json
-  (sleep 20;echo 3; sleep 5; echo 0; sleep 5; echo 'y';sleep 5;echo 4;sleep 5;echo 'wallet.json';sleep 5; expect -c "send \003;") | npm run commands || true
+  #echo $WALLET > wallet.json
+  (sleep 20;echo 3; sleep 5; echo 0; sleep 5; echo 'y';sleep 5;echo 4;sleep 5;echo 'wallet.json';sleep 5;pkill -2 node) | npm run commands || true
   (sleep 20;echo 8;) | npm run commands || true
 fi
