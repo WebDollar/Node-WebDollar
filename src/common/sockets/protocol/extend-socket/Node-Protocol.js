52cb62bec4cf9f35c26336906ace0807f4c9100a
IonutAlexandruPanait <ionutalexpanait@gmail.com>
2019-01-26 01:14:31 +0200

    fix error

--- src/common/sockets/protocol/extend-socket/Node-Protocol.js
+++ src/common/sockets/protocol/extend-socket/Node-Protocol.js
@@ -163,7 +163,7 @@ class NodeProtocol {
 
                 let found = false;
                 for (let j=0; j<exceptSockets.length; j++)
-                    if (exceptSockets[j] !== null && nodes[i].socket.node.sckAddress.matchAddress(exceptSockets[j].node.sckAddress, ["uuid"] )) {
+                    if (typeof exceptSockets[j] !== "undefined" && exceptSockets[j] !== null && nodes[i].socket.node.sckAddress.matchAddress(exceptSockets[j].node.sckAddress, ["uuid"] )) {
                         found = true;
                         break;
                     }
