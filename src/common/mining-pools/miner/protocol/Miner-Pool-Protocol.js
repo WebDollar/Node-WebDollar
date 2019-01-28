78840cd760f68a2e6115e7b30348cbf6b3248055
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2019-01-28 04:47:05 +0200

    bug fix avoid multiple mining

--- src/common/mining-pools/miner/protocol/Miner-Pool-Protocol.js
+++ src/common/mining-pools/miner/protocol/Miner-Pool-Protocol.js
@@ -235,7 +235,8 @@ class MinerProtocol extends PoolProtocolList{
 
                 if (typeof data.work !== "object") throw {message: "new-work invalid work"};
 
-                await this._validateRequestWork(data.work, socket);
+                //await this._validateRequestWork( data.work, socket );
+                this.minerPoolManagement.minerPoolMining.resetForced = true;
 
                 this._updateStatistics( data);
                 this.minerPoolManagement.minerPoolReward.setReward(data);
