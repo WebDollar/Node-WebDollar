7c981a01c569eb6abf1d3b6a5884c6eb67913600
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2019-01-30 16:39:54 +0200

    catch uncaughtException

--- src/build_terminal.js
+++ src/build_terminal.js
@@ -1,9 +1,15 @@
+var process = require('process');
+
 import {Node, Blockchain} from './index';
 import consts from 'consts/const_global';
 import termination from './termination';
 import {JsonRpcServer} from './node/jsonRpc';
 
 JsonRpcServer(consts.JSON_RPC);
+
+process.on('uncaughtException', function (err) {
+    console.error('Caught exception: ' + err);
+});
 
 //                            light-node
 Blockchain.createBlockchain('full-node', ()=>{}, async ()=>{
