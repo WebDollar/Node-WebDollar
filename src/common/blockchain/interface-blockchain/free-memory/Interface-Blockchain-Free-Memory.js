28af9cbb98b8d65e4f05be4bbf7795aebb688bda
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2019-02-13 16:32:07 +0200

    txId & Free Memory optimized

--- src/common/blockchain/interface-blockchain/free-memory/Interface-Blockchain-Free-Memory.js
+++ src/common/blockchain/interface-blockchain/free-memory/Interface-Blockchain-Free-Memory.js
@@ -4,24 +4,27 @@ class InterfaceFreeMemory{
 
         this.blockchain = blockchain;
 
+        this._lastPosition = this.blockchain.blocks.blocksStartingPoint;
+
         setTimeout( this._freeMemoryTimeout.bind(this), 5000 );
     }
 
     async freeMemory(){
 
-        for (let i=this.blockchain.blocks.blocksStartingPoint; i < this.blockchain.blocks.length-50; i++){
+        for (let i=this._lastPosition - 2000; i < this.blockchain.blocks.length-50; i++){
 
             if ( !this.blockchain.blocks[i] ) continue;
 
             this.blockchain.blocks[i].computedBlockPrefix = undefined;
 
-            for (let j=0; j<this.blockchain.blocks[i].data.transactions.transactions.length; j++)
-                delete this.blockchain.blocks[i].data.transactions.transactions[j]._serializated ;
-
-
-            if (i % 10000 === 0) await this.blockchain.sleep(100);
+            for (let j=0; j<this.blockchain.blocks[i].data.transactions.transactions.length; j++) {
+                delete this.blockchain.blocks[i].data.transactions.transactions[j]._serializated;
+                delete this.blockchain.blocks[i].data.transactions.transactions[j]._txId;
+            }
 
         }
+
+        this._lastPosition = this.blockchain.blocks.length-1;
 
     }
 
