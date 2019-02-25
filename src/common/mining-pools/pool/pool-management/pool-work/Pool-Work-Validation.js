b97fb3fa25a0f80f42c8b8ad183970a6b7b5c797
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2019-02-25 03:35:10 +0200

    less cache

--- src/common/mining-pools/pool/pool-management/pool-work/Pool-Work-Validation.js
+++ src/common/mining-pools/pool/pool-management/pool-work/Pool-Work-Validation.js
@@ -159,7 +159,7 @@ class PoolWorkValidation{
             let time = new Date().getTime();
 
             for (let key in this._worksDuplicate)
-                if ( time - this._worksDuplicate[key] > 600000 ){
+                if ( time - this._worksDuplicate[key] > 180000 ){
                     delete this._worksDuplicate[key];
                 }
 
