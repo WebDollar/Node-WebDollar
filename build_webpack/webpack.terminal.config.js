45d82aae90e93c798a3e97102304651316ec4d2c
Ionut Moraru <morion4000@gmail.com>
2019-02-28 16:06:37 +0200

    Update webpack.terminal.config.js

--- build_webpack/webpack.terminal.config.js
+++ build_webpack/webpack.terminal.config.js
@@ -49,6 +49,7 @@ const config = merge(base, {
                 TERMINAL_WORKERS_CPU_MAX: "'" + (process.env.TERMINAL_WORKERS_CPU_MAX || '') + "'",
                 COLLECT_STATS: process.env.COLLECT_STATS,
                 GH_COMMIT: "'" + (process.env.GH_COMMIT || '') + "'",
+                LOG_LEVEL: "'" + (process.env.LOG_LEVEL || '') + "'",
             }
         })
     ]
