b6fac7c3b2917e11995809a09b75c1e3df43bda5
IonutAlexandruPanait <ionutalexpanait@gmail.com>
2019-08-20 15:19:56 +0300

    update version

--- src/consts/const_global.js
+++ src/consts/const_global.js
@@ -279,7 +279,7 @@ consts.SETTINGS = {
 
     NODE: {
 
-        VERSION: "1.211.5",
+        VERSION: "1.300.0",
 
         VERSION_COMPATIBILITY: "1.200.2",
         VERSION_COMPATIBILITY_POOL_MINERS: "1.200.1",
