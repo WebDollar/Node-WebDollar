2fd3ce75bc39ab1415f1afddd600de12e6c1b2d8
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2019-12-20 15:18:36 +0200

    getTransaction API route

--- src/node/sockets/node-server/API/public/Node-API-Public-Transactions.js
+++ src/node/sockets/node-server/API/public/Node-API-Public-Transactions.js
@@ -26,6 +26,35 @@ class NodeAPIPublicTransactions {
       return {result: false, message: exception.message}
     }
 
+  }
+
+  async getTransaction(req, res){
+
+    try{
+
+      let txId = req.tx_id;
+      if ( typeof txId !== "string" || txId.length !== 64 ) throw {message: "Invalid Tx Id"}
+
+      let answer = await Blockchain.Transactions.checkVirtualizedTxId( req.tx_id );
+
+      if (!!answer){
+
+        const block = await Blockchain.blockchain.getBlock(answer);
+
+        const txIndex = block.data.transactions.findTransactionInBlockData(txId);
+
+        if (txIndex === -1) throw {message: "Transaction not found in block"};
+
+        const tx = block.data.transactions.transactions[txIndex];
+
+        return { result: true, tx: tx.toJSON() };
+
+      } else
+        throw {result: false, message: "Transaction not found"};
+
+    }catch(exception){
+      return {result: false, message: exception.message}
+    }
 
   }
 
