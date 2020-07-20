a23e5c57cd1282a51fd51f01caa284eae3497f4c
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2020-07-20 19:42:08 +0300

    new version 1.3.02

--- src/consts/const_global.js
+++ src/consts/const_global.js
@@ -267,7 +267,7 @@ consts.SETTINGS = {
 
     NODE: {
 
-        VERSION: "1.3.01",
+        VERSION: "1.3.02",
 
         VERSION_COMPATIBILITY: "1.200.3",
         VERSION_COMPATIBILITY_POOL_MINERS: "1.200.1",
