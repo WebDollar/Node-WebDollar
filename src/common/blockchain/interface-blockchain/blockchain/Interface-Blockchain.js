bd6cd9ced78a16c1ce6b942924681591be7492d6
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2020-08-03 12:45:18 +0300

    more fixes for POS100 and prepare a new testnet

--- src/common/blockchain/interface-blockchain/blockchain/Interface-Blockchain.js
+++ src/common/blockchain/interface-blockchain/blockchain/Interface-Blockchain.js
@@ -186,7 +186,7 @@ class InterfaceBlockchain extends InterfaceBlockchainBasic{
             if (height < consts.BLOCKCHAIN.HARD_FORKS.POS90_ACTIVATION) {
                 if (height % 30 === 29) height = height - 10;  //first POS, get the last proof of Stake
                 else if (height % 30 === 19) height = height - 20; //first POW, get the last proof of Work
-            } else {
+            } else if (height < consts.BLOCKCHAIN.HARD_FORKS.POS100_ACTIVATION) {
                 if (height % 100 === 99) height = height - 10; //first POS, get the last proof of Stake
                 else if (height % 100 === 89) height = height - 90; //first POW, get the last proof of work
             }
