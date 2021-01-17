b354dee2c94d01f7221f23157879533c92b8c216
IonutAlexandruPanait <ionutalexpanait@gmail.com>
2021-01-17 16:29:56 +0200

    fix pool

--- src/common/mining-pools/miner/Miner-Pool-Settings.js
+++ src/common/mining-pools/miner/Miner-Pool-Settings.js
@@ -233,7 +233,7 @@ class MinerPoolSettings {
         await this.addPoolList("/pool/1/Balanel_si_Miaunel/0.02/cd7217ad76118df5357ae7a094aa48096daae8a67767bd3acbc8638dc68955ac/https:$$webd.pool.coffee:8443", undefined, true);
         await this.addPoolList("pool/1/EuroPool/0.02/f06b4720a725eb4fe13c06b26fca4c862b2f2f2ddc831e411418aceefae8e6df/https:$$webd-europool.ddns.net:2222", undefined, true);
         await this.addPoolList("/pool/1/WMP/0.02/d02e26e60a5b0631a0b71e7dc72bb7492fd018dad64531498df369ec14f87962/https:$$server.webdollarminingpool.com:443", undefined, true);
-        await this.addPoolList("/pool/1/Timi/0.001/0fc73835374ee10ae3bf7f1f404d435a18849c4ab131d8a14a85acb4a1068260/https:$$pool.timi.ro", undefined, true);
+        await this.addPoolList("/pool/1/Timi/0.001/0fc73835374ee10ae3bf7f1f404d435a18849c4ab131d8a14a85acb4a1068260/https:$$pool.timi.ro:443", undefined, true);
         await this.addPoolList("/pool/1/WMP-ASIA/0.02/773a8d5f7ce3c0dba0b7216c35d2768b1a6abef716fa2a46d875d6ca0e2c115e/https:$$singapore.webdollarminingpool.com:443", undefined, true);
     }
 
