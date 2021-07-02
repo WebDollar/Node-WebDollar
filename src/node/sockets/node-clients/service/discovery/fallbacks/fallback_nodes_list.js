0537db81cdbf3e5a4816d69be3c911518a0a1d95
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2021-07-02 16:58:49 +0300

    Revert fallback changes

--- src/node/sockets/node-clients/service/discovery/fallbacks/fallback_nodes_list.js
+++ src/node/sockets/node-clients/service/discovery/fallbacks/fallback_nodes_list.js
@@ -9,119 +9,116 @@ export default {
         //--------------Community FallBack Nodes-------------------
         //---------------------------------------------------------
 
-        {"addr": ["http://helloworldx.ddns.net:8080"]},
-        {"addr": ["http://mryoung79.de:3333"]},
-        {"addr": ["http://188.24.107.165:8080"]},
 
-        // {"addr": ["https://webd.5q.ro:8443"]}, // Thanks to Sorin M
-        //
-        // //{"addr": ["https://ez.cnbacolt.com:443"]},
-        // //{"addr": ["https://server.cnbacolt.com:443"]},
-        //
-        // //{"addr": ["https://webdollar-vps1.ddns.net:80"]},
-        //
-        // {"addr": ["https://node1.petreus.ro:443"]}, // Thanks to Dani Petreus
-        // {"addr": ["https://node2.petreus.ro:443"]}, // Thanks to Dani Petreus
-        // {"addr": ["https://node3.petreus.ro:443"]}, // Thanks to Dani Petreus
-        // {"addr": ["https://node4.petreus.ro:443"]}, // Thanks to Dani Petreus
-        // {"addr": ["https://node5.petreus.ro:443"]}, // Thanks to Dani Petreus
-        // {"addr": ["https://node6.petreus.ro:443"]}, // Thanks to Dani Petreus
-        // {"addr": ["https://node7.petreus.ro:443"]}, // Thanks to Dani Petreus
-        // {"addr": ["https://node8.petreus.ro:443"]}, // Thanks to Dani Petreus
-        // {"addr": ["https://node9.petreus.ro:443"]}, // Thanks to Dani Petreus
-        // {"addr": ["https://node10.petreus.ro:443"]}, // Thanks to Dani Petreus
-        //
-        // {"addr": ["https://node.timi.ro:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node1.timi.ro:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node2.timi.ro:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node3.timi.ro:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node4.timi.ro:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node5.timi.ro:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node6.timi.ro:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node7.timi.ro:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node8.timi.ro:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node9.timi.ro:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node10.timi.ro:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node11.timi.ro:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node12.timi.ro:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node13.timi.ro:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node14.timi.ro:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node15.timi.ro:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node16.timi.ro:443"]}, // Thanks to @TimiDani
-        //
-        // {"addr": ["https://node1.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node2.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node3.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node4.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node5.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node6.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node7.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node8.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node9.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node10.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node11.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node12.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node13.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node14.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
-        // {"addr": ["https://node15.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
-        //
-        // //{"addr": ["https://webdollarpool.win:80/"]}, // Thanks to @vladimirpetre
-        // //{"addr": ["https://us.webdollarpool.win:80/"]}, // Thanks to @vladimirpetre
-        // //{"addr": ["https://losangeles.webdollarpool.win:80/"]}, // Thanks to @vladimirpetre
-        // //{"addr": ["https://singapore.webdollarpool.win:80/"]}, // Thanks to @vladimirpetre
-        //
-        // {"addr": ["https://falx.romeonet.ro:65001"]}, // Thanks to @romeonet
-        // {"addr": ["https://romeonet.ddns.net:65101"]}, // Thanks to @romeonet
-        //
-        // //{"addr": ["https://node-eu.int-webd.com:5001"]}, // Thanks to @int_webd
-        // //{"addr": ["https://node-eu.int-webd.com:5002"]}, // Thanks to @int_webd
-        // //{"addr": ["https://node-eu.int-webd.com:5003"]}, // Thanks to @int_webd
-        // //{"addr": ["https://node-eu.int-webd.com:5004"]}, // Thanks to @int_webd
-        // //{"addr": ["https://node-eu.int-webd.com:5005"]}, // Thanks to @int_webd
-        // //{"addr": ["https://node-eu.int-webd.com:5006"]}, // Thanks to @int_webd
-        // //{"addr": ["https://node-eu.int-webd.com:5007"]}, // Thanks to @int_webd
-        // //{"addr": ["https://node-eu.int-webd.com:5008"]}, // Thanks to @int_webd
-        // //{"addr": ["https://node-eu.int-webd.com:5009"]}, // Thanks to @int_webd
-        // //{"addr": ["https://node-eu.int-webd.com:5010"]}, // Thanks to @int_webd
-        //
-        // {"addr": ["https://cryptocoingb.ddns.net:8080"]}, // Thanks to CryptoCoinGB
-        // {"addr": ["https://cryptocoingb.ddns.net:8081"]}, // Thanks to CryptoCoinGB
-        //
-        // //{"addr": ["https://bacm.ro:443"]}, //Thanks to @jigodia
-        //
-        //
-        // //---------------------------------------------------------
-        // //--------------WebDollar FallBack Nodes-------------------
-        // //---------------------------------------------------------
-        //
-        // //{"addr": ["https://webdchucknorris.vpnromania.ro:80"]}, // Thanks to @cbusuioceanu
-        // //{"addr": ["https://webdchucknorris.vpnromania.ro:8080"]}, // Thanks to @cbusuioceanu
-        // //{"addr": ["https://webdchucknorris.vpnromania.ro:8081"]}, // Thanks to @cbusuioceanu
-        // //{"addr": ["https://webdchucknorris.vpnromania.ro:8082"]}, // Thanks to @cbusuioceanu
-        // //{"addr": ["https://webdchucknorris.vpnromania.ro:8083"]}, // Thanks to @cbusuioceanu
-        // //{"addr": ["https://webdchucknorris.vpnromania.ro:8084"]}, // Thanks to @cbusuioceanu
-        // //{"addr": ["https://webdchucknorris.vpnromania.ro:8085"]}, // Thanks to @cbusuioceanu
-        // //{"addr": ["https://webdchucknorris.vpnromania.ro:8086"]}, // Thanks to @cbusuioceanu
-        // //{"addr": ["https://webdchucknorris.vpnromania.ro:8087"]}, // Thanks to @cbusuioceanu
-        // //{"addr": ["https://webdchucknorris.vpnromania.ro:8088"]}, // Thanks to @cbusuioceanu
-        // //{"addr": ["https://webdchucknorris.vpnromania.ro:8089"]}, // Thanks to @cbusuioceanu
-        // //{"addr": ["https://webdchucknorris.vpnromania.ro:8090"]}, // Thanks to @cbusuioceanu
-        // //{"addr": ["https://webdchucknorris.vpnromania.ro:8091"]}, // Thanks to @cbusuioceanu
-        // //{"addr": ["https://webdchucknorris.vpnromania.ro:8092"]}, // Thanks to @cbusuioceanu
-        // //{"addr": ["https://webdchucknorris.vpnromania.ro:8093"]}, // Thanks to @cbusuioceanu
-        // //{"addr": ["https://webdchucknorris.vpnromania.ro:8094"]}, // Thanks to @cbusuioceanu
-        // //{"addr": ["https://webdchucknorris.vpnromania.ro:8095"]}, // Thanks to @cbusuioceanu
-        // //{"addr": ["https://webdchucknorris.vpnromania.ro:8096"]}, // Thanks to @cbusuioceanu
-        // //{"addr": ["https://webdchucknorris.vpnromania.ro:8097"]}, // Thanks to @cbusuioceanu
-        // //{"addr": ["https://webdchucknorris.vpnromania.ro:8098"]}, // Thanks to @cbusuioceanu
-        // //{"addr": ["https://webdchucknorris.vpnromania.ro:8099"]}, // Thanks to @cbusuioceanu
-        // //{"addr": ["https://webdchucknorris.vpnromania.ro:8100"]}, // Thanks to @cbusuioceanu
-        //
-        //
-        // //{"addr": ["https://webdollar.csland.ro:8440"]}, // Thanks to @mariotheodor
-        // //{"addr": ["https://webdollar.csland.ro:8441"]}, // Thanks to @mariotheodor
-        // //{"addr": ["https://webdollar.csland.ro:8442"]}, // Thanks to @mariotheodor
-        // //{"addr": ["https://webdollar.csland.ro:8443"]}, // Thanks to @mariotheodor
+        {"addr": ["https://webd.5q.ro:8443"]}, // Thanks to Sorin M
+
+        //{"addr": ["https://ez.cnbacolt.com:443"]},
+        //{"addr": ["https://server.cnbacolt.com:443"]},
+
+        //{"addr": ["https://webdollar-vps1.ddns.net:80"]},
+
+        {"addr": ["https://node1.petreus.ro:443"]}, // Thanks to Dani Petreus
+        {"addr": ["https://node2.petreus.ro:443"]}, // Thanks to Dani Petreus
+        {"addr": ["https://node3.petreus.ro:443"]}, // Thanks to Dani Petreus
+        {"addr": ["https://node4.petreus.ro:443"]}, // Thanks to Dani Petreus
+        {"addr": ["https://node5.petreus.ro:443"]}, // Thanks to Dani Petreus
+        {"addr": ["https://node6.petreus.ro:443"]}, // Thanks to Dani Petreus
+        {"addr": ["https://node7.petreus.ro:443"]}, // Thanks to Dani Petreus
+        {"addr": ["https://node8.petreus.ro:443"]}, // Thanks to Dani Petreus
+        {"addr": ["https://node9.petreus.ro:443"]}, // Thanks to Dani Petreus
+        {"addr": ["https://node10.petreus.ro:443"]}, // Thanks to Dani Petreus
+
+        {"addr": ["https://node.timi.ro:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node1.timi.ro:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node2.timi.ro:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node3.timi.ro:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node4.timi.ro:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node5.timi.ro:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node6.timi.ro:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node7.timi.ro:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node8.timi.ro:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node9.timi.ro:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node10.timi.ro:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node11.timi.ro:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node12.timi.ro:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node13.timi.ro:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node14.timi.ro:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node15.timi.ro:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node16.timi.ro:443"]}, // Thanks to @TimiDani
+
+        {"addr": ["https://node1.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node2.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node3.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node4.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node5.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node6.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node7.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node8.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node9.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node10.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node11.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node12.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node13.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node14.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
+        {"addr": ["https://node15.webdollarminingpool.com:443"]}, // Thanks to @TimiDani
+
+        //{"addr": ["https://webdollarpool.win:80/"]}, // Thanks to @vladimirpetre
+        //{"addr": ["https://us.webdollarpool.win:80/"]}, // Thanks to @vladimirpetre
+        //{"addr": ["https://losangeles.webdollarpool.win:80/"]}, // Thanks to @vladimirpetre
+        //{"addr": ["https://singapore.webdollarpool.win:80/"]}, // Thanks to @vladimirpetre
+
+        {"addr": ["https://falx.romeonet.ro:65001"]}, // Thanks to @romeonet
+        {"addr": ["https://romeonet.ddns.net:65101"]}, // Thanks to @romeonet
+
+        //{"addr": ["https://node-eu.int-webd.com:5001"]}, // Thanks to @int_webd
+        //{"addr": ["https://node-eu.int-webd.com:5002"]}, // Thanks to @int_webd
+        //{"addr": ["https://node-eu.int-webd.com:5003"]}, // Thanks to @int_webd
+        //{"addr": ["https://node-eu.int-webd.com:5004"]}, // Thanks to @int_webd
+        //{"addr": ["https://node-eu.int-webd.com:5005"]}, // Thanks to @int_webd
+        //{"addr": ["https://node-eu.int-webd.com:5006"]}, // Thanks to @int_webd
+        //{"addr": ["https://node-eu.int-webd.com:5007"]}, // Thanks to @int_webd
+        //{"addr": ["https://node-eu.int-webd.com:5008"]}, // Thanks to @int_webd
+        //{"addr": ["https://node-eu.int-webd.com:5009"]}, // Thanks to @int_webd
+        //{"addr": ["https://node-eu.int-webd.com:5010"]}, // Thanks to @int_webd
+
+        {"addr": ["https://cryptocoingb.ddns.net:8080"]}, // Thanks to CryptoCoinGB
+        {"addr": ["https://cryptocoingb.ddns.net:8081"]}, // Thanks to CryptoCoinGB
+
+        //{"addr": ["https://bacm.ro:443"]}, //Thanks to @jigodia
+
+
+        //---------------------------------------------------------
+        //--------------WebDollar FallBack Nodes-------------------
+        //---------------------------------------------------------
+
+        //{"addr": ["https://webdchucknorris.vpnromania.ro:80"]}, // Thanks to @cbusuioceanu
+        //{"addr": ["https://webdchucknorris.vpnromania.ro:8080"]}, // Thanks to @cbusuioceanu
+        //{"addr": ["https://webdchucknorris.vpnromania.ro:8081"]}, // Thanks to @cbusuioceanu
+        //{"addr": ["https://webdchucknorris.vpnromania.ro:8082"]}, // Thanks to @cbusuioceanu
+        //{"addr": ["https://webdchucknorris.vpnromania.ro:8083"]}, // Thanks to @cbusuioceanu
+        //{"addr": ["https://webdchucknorris.vpnromania.ro:8084"]}, // Thanks to @cbusuioceanu
+        //{"addr": ["https://webdchucknorris.vpnromania.ro:8085"]}, // Thanks to @cbusuioceanu
+        //{"addr": ["https://webdchucknorris.vpnromania.ro:8086"]}, // Thanks to @cbusuioceanu
+        //{"addr": ["https://webdchucknorris.vpnromania.ro:8087"]}, // Thanks to @cbusuioceanu
+        //{"addr": ["https://webdchucknorris.vpnromania.ro:8088"]}, // Thanks to @cbusuioceanu
+        //{"addr": ["https://webdchucknorris.vpnromania.ro:8089"]}, // Thanks to @cbusuioceanu
+        //{"addr": ["https://webdchucknorris.vpnromania.ro:8090"]}, // Thanks to @cbusuioceanu
+        //{"addr": ["https://webdchucknorris.vpnromania.ro:8091"]}, // Thanks to @cbusuioceanu
+        //{"addr": ["https://webdchucknorris.vpnromania.ro:8092"]}, // Thanks to @cbusuioceanu
+        //{"addr": ["https://webdchucknorris.vpnromania.ro:8093"]}, // Thanks to @cbusuioceanu
+        //{"addr": ["https://webdchucknorris.vpnromania.ro:8094"]}, // Thanks to @cbusuioceanu
+        //{"addr": ["https://webdchucknorris.vpnromania.ro:8095"]}, // Thanks to @cbusuioceanu
+        //{"addr": ["https://webdchucknorris.vpnromania.ro:8096"]}, // Thanks to @cbusuioceanu
+        //{"addr": ["https://webdchucknorris.vpnromania.ro:8097"]}, // Thanks to @cbusuioceanu
+        //{"addr": ["https://webdchucknorris.vpnromania.ro:8098"]}, // Thanks to @cbusuioceanu
+        //{"addr": ["https://webdchucknorris.vpnromania.ro:8099"]}, // Thanks to @cbusuioceanu
+        //{"addr": ["https://webdchucknorris.vpnromania.ro:8100"]}, // Thanks to @cbusuioceanu
+
+
+        //{"addr": ["https://webdollar.csland.ro:8440"]}, // Thanks to @mariotheodor
+        //{"addr": ["https://webdollar.csland.ro:8441"]}, // Thanks to @mariotheodor
+        //{"addr": ["https://webdollar.csland.ro:8442"]}, // Thanks to @mariotheodor
+        //{"addr": ["https://webdollar.csland.ro:8443"]}, // Thanks to @mariotheodor
 
     ],
 
