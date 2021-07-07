38c4e1c31afb27f502735df334ca17eb93d156ea
MartinCanto <joeymartin77@hotmail.com>
2021-07-07 08:59:56 -0400

    Update start_pool.sh
    Pass environment variables to create a new pool.
    $WALLET - Your pool wallet
    $FEE - The fee your pool charges
    $POOLNAME - The name of your pool
    $POOLURL -  The URL of your pool
    $NEWPOOL - If you have an existing poolDB that you plan to mount inside the container then you can set this to false and it will just start up the previous pool

--- start_pool.sh
+++ start_pool.sh
@@ -1,1 +1,12 @@
-(sleep 60;echo 11;sleep 5;echo 2;sleep 5;echo 'testnet';sleep 5;echo 'http://google.com';sleep 5;echo 2;sleep 5;echo 'n') | npm run commands || true
+if [ "$NEWPOOL" = "false" ]
+then
+  echo $WALLET > wallet.json
+  (sleep 60;echo 4;sleep 5;echo 'wallet.json';sleep 5;echo 7;sleep 5;echo 1;sleep 5;echo 3;sleep 5;echo 0;sleep 5;echo 'y';sleep 5;echo exit;) | npm run commands || true 
+  rm -rf ./wallet.json
+  (sleep 60;echo 11;sleep 5;echo 'y';sleep 5) | npm run commands || true 
+else
+  echo $WALLET > wallet.json
+  (sleep 60;echo 4;sleep 5;echo 'wallet.json';sleep 5;echo 7;sleep 5;echo 1;sleep 5;echo 3;sleep 5;echo 0;sleep 5;echo 'y';sleep 5;echo exit;) | npm run commands || true 
+  rm -rf ./wallet.json
+  (sleep 60;echo 11;sleep 5;echo $FEE;sleep 5;echo $POOLNAME;sleep 5;echo $POOLURL;sleep 5;echo $REFERAL;sleep 5;echo 'n';sleep 5) | npm run commands || true 
+fi
