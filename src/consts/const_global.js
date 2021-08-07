3d0c1f798bc602f4c2a804b1532ce183346b874b
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2021-08-08 00:06:32 +0300

    const global changed to allow more sockets

--- src/consts/const_global.js
+++ src/consts/const_global.js
@@ -333,8 +333,8 @@ consts.SETTINGS = {
                     MIN_SOCKET_CLIENTS_WAITLIST_FALLBACK: 2,
 
                     SERVER_OPEN:{
-                        MAX_SOCKET_CLIENTS_WAITLIST: 5,
-                        MAX_SOCKET_CLIENTS_WAITLIST_FALLBACK: 3,
+                        MAX_SOCKET_CLIENTS_WAITLIST: 10,
+                        MAX_SOCKET_CLIENTS_WAITLIST_FALLBACK: 10,
                     },
 
                     SSL:{
