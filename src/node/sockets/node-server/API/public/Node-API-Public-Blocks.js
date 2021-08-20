2a37320459d333bdedd40afe1a5c7386ecc71936
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2021-08-20 17:37:09 +0300

    minor update

--- src/node/sockets/node-server/API/public/Node-API-Public-Blocks.js
+++ src/node/sockets/node-server/API/public/Node-API-Public-Blocks.js
@@ -14,19 +14,21 @@ class NodeAPIPublicBlocks{
             const startMin = Math.max(0, Blockchain.blockchain.blocks.length - LIMIT_BLOCKS);
             const endMax = Blockchain.blockchain.blocks.length;
 
-            let block_start = req.block_start || startMin;
-            let block_end = req.block_end || endMax;
+            let block_start = Number.parseInt(req.block_start) || startMin;
+            let block_end = Number.parseInt(req.block_end) || endMax;
 
             if (block_start < 0 || block_start > endMax) throw {message: "block start is not correct: " + block_start};
             if (block_end < 0 || block_end > endMax ) throw {message: "block end is not correct: " + block_start};
 
             if (block_end - block_start > LIMIT_BLOCKS) throw {message: "requested too many blocks: " + block_end - block_start  };
 
-            const blocks_to_send = [];
+            let blocks_to_send = [];
             for (let i=block_start; i<block_end; i++)
-                blocks_to_send.push( (await Blockchain.blockchain.getBlock(i)).toJSON() )
+                blocks_to_send.push( Blockchain.blockchain.getBlock(i) )
 
-            return {result: true, blocks: blocks_to_send};
+            blocks_to_send = await Promise.all(blocks_to_send)
+
+            return {result: true, blocks: blocks_to_send.map( it => it.toJSON() ) };
 
         } catch (exception) {
             return {result: false, message: exception.message};
